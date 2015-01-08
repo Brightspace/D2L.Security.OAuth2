6134fb8ed6885a08f929aee0b7f965b9c81b3fa3
Dave Fallis <Dave.Fallis@d2l.com>
2015-01-08 10:39:20 -0500

    Stub implementation

--- D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/Principal.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/Principal.cs
@@ -1,9 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace D2L.Security.AuthTokenValidation {
 
-	public abstract class Principal {
+	public sealed class Principal {
+
+		public Principal(
+			long userId,
+			string tenantId,
+			string xsrfToken,
+			HashSet<string> scopes
+			) {
+
+			UserId = userId;
+			TenantId = tenantId;
+			XsrfToken = xsrfToken;
+			Scopes = scopes;
+		}
+
+		public HashSet<string> Scopes { get; private set; }
+
+		public long UserId { get; private set; }
+
+		public string TenantId { get; private set; }
+
+		public string XsrfToken { get; private set; }
+
+		public bool IsBrowserUser { get { return XsrfToken != null; } }
 
 		public bool HasScope( string scope ) {
 			return Scopes.Contains( "*" ) || Scopes.Contains( scope );
@@ -14,7 +36,5 @@ namespace D2L.Security.AuthTokenValidation {
 				throw new AuthorizationException( string.Format( "Not authorized for scope '{0}'", scope ) );
 			}
 		}
-
-		public HashSet<string> Scopes { get; set; }
 	}
 }
