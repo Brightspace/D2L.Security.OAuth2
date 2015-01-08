04e7e588b25d9e63d347c73884ec3dad5b4f1854
Dave Fallis <Dave.Fallis@d2l.com>
2015-01-08 16:07:12 -0500

    Added auth service endpoint to factory Create method

--- D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/AuthTokenValidatorFactory.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/AuthTokenValidatorFactory.cs
@@ -1,12 +1,15 @@
-﻿using D2L.Security.AuthTokenValidation.Default;
+﻿using System;
+using D2L.Security.AuthTokenValidation.Default;
 
 namespace D2L.Security.AuthTokenValidation {
 
 	public sealed class AuthTokenValidatorFactory : IAuthTokenValidatorFactory {
 
-		public IAuthTokenValidator Create() {
+		public IAuthTokenValidator Create(
+			Uri authServiceEndpoint
+			) {
 			return new AuthTokenValidator(
-				new AuthServerPublicKeyProvider()
+				new AuthServerPublicKeyProvider( authServiceEndpoint )
 				);
 		}
 	}
--- D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/Default/AuthServerPublicKeyProvider.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/Default/AuthServerPublicKeyProvider.cs
@@ -4,6 +4,15 @@ using System.Security.Cryptography;
 namespace D2L.Security.AuthTokenValidation.Default {
 
 	internal sealed class AuthServerPublicKeyProvider : IAuthServerPublicKeyProvider {
+
+		private readonly Uri m_authServiceEndpoint;
+
+		public AuthServerPublicKeyProvider(
+			Uri authServiceEndpoint
+			) {
+			m_authServiceEndpoint = authServiceEndpoint;
+		}
+
 		public CngKey Get() {
 			throw new NotImplementedException();
 		}
