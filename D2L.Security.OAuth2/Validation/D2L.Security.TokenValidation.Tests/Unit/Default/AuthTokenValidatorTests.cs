87a17b53918fa1168d65f3ece2dddbbac6c8f38d
Dave Fallis <Dave.Fallis@d2l.com>
2015-01-08 16:21:42 -0500

    Added test project, removed dead files

---
+++ D2L.Security.OAuth2/Validation/D2L.Security.TokenValidation.Tests/Unit/Default/AuthTokenValidatorTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace D2L.Security.TokenValidation.Tests.Unit.Default {
+
+	[TestFixture]
+	internal sealed class AuthTokenValidatorTests {
+
+		[Test]
+		public void Test1() {
+			
+		}
+	}
+}
