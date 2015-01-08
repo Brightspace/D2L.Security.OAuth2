a7ad5fc868cc2dba8dd0c8d001814bc9bfb03e08
Dave Fallis <Dave.Fallis@d2l.com>
2015-01-08 10:39:20 -0500

    Stub implementation

---
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/2publishNugetPackage.bat
@@ -0,0 +1,3 @@
+..\.nuget\nuget push D2L.Security.AuthTokenValidation.1.0.0.0.nupkg REDACTED-nuget-publish-key -s http://nuget.build.d2l/nuget/stable
+
+pause
--- D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/Default/AuthTokenChecker.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/Default/AuthTokenChecker.cs
@@ -1,14 +1,16 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 
 namespace D2L.Security.AuthTokenValidation.Default {
 
 	public sealed class AuthTokenChecker : IAuthTokenChecker {
 
 		public Principal VerifyAndDecode( HttpRequest request ) {
-			
+			throw new NotImplementedException();
 		}
 
 		public Principal VerifyAndDecode( string jwt ) {
+			throw new NotImplementedException();
 		}
 	}
 }
