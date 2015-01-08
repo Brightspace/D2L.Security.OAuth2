b56656cf32cbc145e3696876117e2e53c172bbeb
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-08 17:24:36 -0500

    Barebones certificate retrieval

---
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/PublicKeys/Implementations/DefaultPublicKeyProvider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using D2L.Security.AuthTokenValidation.Utilities;
+using Microsoft.IdentityModel.Protocols;
+
+namespace D2L.Security.AuthTokenValidation.PublicKeys.Implementations {
+	class DefaultPublicKeyProvider {
+
+		private readonly HttpMessageHandler _backchannelHttpHandler;
+		private readonly ConfigurationManager<OpenIdConnectConfiguration> _configurationManager;
+
+		private string _issuer;
+		private IEnumerable<SecurityToken> _tokens;
+
+		public DefaultPublicKeyProvider( 
+			string authority,
+			HttpMessageHandler backchannelHttpHandler
+			) {
+
+			authority = FormatAuthority( authority );
+
+			_backchannelHttpHandler = backchannelHttpHandler ?? new WebRequestHandler();
+
+			_configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>( authority, new HttpClient( _backchannelHttpHandler ) );
+		}
+
+		private string FormatAuthority( string authority ) {
+			if( !authority.EndsWith( "/" ) ) {
+				authority += "/";
+			}
+			authority += ".well-known/openid-configuration";
+			return authority;
+		}
+
+		private void RetrieveMetadata() {
+			var result = AsyncHelper.RunSync<OpenIdConnectConfiguration>( async () => await _configurationManager.GetConfigurationAsync() );
+			var tokens = from key in result.JsonWebKeySet.Keys
+						 select new X509SecurityToken( new X509Certificate2( Convert.FromBase64String( key.X5c.First() ) ) );
+
+			_issuer = result.Issuer;
+			_tokens = tokens;
+		}
+	}
+}
