614be9186de8e44ded15be733f9a8f9be713756a
Jacob Parker <Jacob.Parker@D2L.com>
2015-01-08 12:38:07 -0500

    Initial commit

---
+++ BrowserAuthTokens/Default/TokenProvider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BrowserAuthTokens.Default {
+	public sealed class TokenProvider : ITokenProvider {
+		public string TryGetTokenForUser( long userId, long duration ) {
+			throw new NotImplementedException();
+		}
+	}
+}
