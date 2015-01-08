b4587eadf3faf06cbbce29e39ecd52f0eee99d98
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-08 18:00:37 -0500

    Small test which proves public key fetching (for JWT token validation)

---
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation.Tests/Integration/TemporaryTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D2L.Security.AuthTokenValidation.PublicKeys.Implementations;
+using NUnit.Framework;
+
+namespace D2L.Security.AuthTokenValidation.Tests.Integration {
+	
+	[TestFixture]
+	class TemporaryTest {
+
+		private const string AUTH_SERVER = "https://phwinsl01.proddev.d2l:44333/core/";
+
+		[Test]
+		public void TestConnectivity() {
+			DefaultPublicKeyProvider provider = new DefaultPublicKeyProvider( AUTH_SERVER );
+			SecurityToken[] tokens = provider._tokens.ToArray();
+		}
+	}
+}
