a6ebe811f89ea1404cc4831967cafc051cf91b97
Dave Fallis <Dave.Fallis@d2l.com>
2015-01-09 17:38:13 -0500

    US43364 Added tests

--- D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation.Tests/Unit/Default/AuthTokenValidatorTests.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation.Tests/Unit/Default/AuthTokenValidatorTests.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Security.Cryptography;
-using System.Text;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Reflection;
+using System.Web;
 using D2L.Security.AuthTokenValidation.Default;
-using Moq;
 using NUnit.Framework;
 
 namespace D2L.Security.AuthTokenValidation.Tests.Unit.Default {
@@ -10,19 +11,157 @@ namespace D2L.Security.AuthTokenValidation.Tests.Unit.Default {
 	[TestFixture]
 	internal sealed class AuthTokenValidatorTests {
 
+		private const string TOKEN_STUB = "abcde";
+
+		private readonly IAuthTokenValidator m_authTokenValidator = new AuthTokenValidator( null );
+
+		#region VerifyHeader Tests
+
 		[Test]
-		public void Test() {
+		public void VerifyHeader_ValidHeader_Succeeds() {
 
-			//string key = "MIIC/DCCAeigAwIBAgIQBSZNe52PDaNMFh5n8I0gBzAJBgUrDgMCHQUAMBcxFTATBgNVBAMTDGF1dGguZGV2LmQybDAeFw0xNDA4MDEyMTM0NTFaFw0yMDAxMDEwNDAwMDBaMBcxFTATBgNVBAMTDGF1dGguZGV2LmQybDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAJ/juR071Q7E26k9+LCYCTxvwbrxPN2qnOAoQihU0HK8KMiVWfU9lyVIwHUrjS3e6n++/OKxgZnsR1paFnKbNRNP1HDq+RtBEBaSPYV5HrkD11SdzSU5Z1tWp/FVuZ15Utm+TzArkz3/1508km0yb4J0Rw48gyc+q1mc3ZnTYTP2jK9a1xN9xXDOxciiedZVyuLCdhA3at/VGZGo1v6+eN9iXGO0CplGyJPD/mRZvKrkJPY1GndFeEImu+6rZ+Oj/76PQTCz6L3D2WOOYZkK1LdC9IMCYgV3YmyBHewiu1WsmBtwl27LMUvJIeuU8xtVFXIS4bWOvO4cX/zaUxgTarkCAwEAAaNMMEowSAYDVR0BBEEwP4AQ8ZbpvXP8fwuZdg1earlZE6EZMBcxFTATBgNVBAMTDGF1dGguZGV2LmQybIIQBSZNe52PDaNMFh5n8I0gBzAJBgUrDgMCHQUAA4IBAQBIfvG38kIhQ8Jjy24cVLmLxtmpEEBbgfODSkG4aGFctA+BDpxP1anjY/nYedssFcaDuFHNvJqNqL1vx2YpSST10UyjzD+TnfXPl+ssgpxDcBIeiS5Q0T5AdmFkJVUER5KAHxB3NuPitqODNKMW5I7dCQ2wM9cmttS6JkfrTjCv/SkSEXh9GYBItGqIk1n1Mbkx8nTEc4OSTHau01ofWx+d6+lvEKkIu5Blw3ZmWnLIGfWY+JQtFe8ldfgXHWCh3+wdaDdKhSB3ZLLdfz7zO3Cfg4L+/DFzkK0wLny/jF9yGfqqXv931L7gt75kGrIqXL28C5xErXUa5KPVFqN4xQKU";
+			string jwtHeader = MakeJwtHeader( "RS256", "JWT" );
 
-			//byte[] bytes = new byte[ key.Length * sizeof(char) ];
-			//Buffer.BlockCopy( key.ToCharArray(), 0, bytes, 0, bytes.Length );
-			//CngKey.
-			//CngKey cngKey = CngKey.Import( bytes, new CngKeyBlobFormat() )
+			AuthTokenValidator.VerifyHeader( jwtHeader );
 
-			//Mock<IAuthServerPublicKeyProvider> keyProviderMock = new Mock<IAuthServerPublicKeyProvider>();
-			//keyProviderMock.Setup( kp => kp.Get() ).Returns( new  );
-			//IAuthTokenValidator atv = new AuthTokenValidator();
+			Assert.Pass();
+		}
+
+		[Test]
+		public void VerifyHeader_WrongAlgorithm_Exception() {
+
+			string jwtHeader = MakeJwtHeader( "ABCDE", "JWT" );
+			Assert.Throws<AuthorizationException>( () => AuthTokenValidator.VerifyHeader( jwtHeader ) );
+		}
+
+		[Test]
+		public void VerifyHeader_WrongType_Exception() {
+
+			string jwtHeader = MakeJwtHeader( "RS256", "ABC" );
+			Assert.Throws<AuthorizationException>( () => AuthTokenValidator.VerifyHeader( jwtHeader ) );
+		}
+
+		[Test]
+		public void VerifyHeader_WrongTypeAndAlgorithm_Exception() {
+
+			string jwtHeader = MakeJwtHeader( "ABCDE", "ABC" );
+			Assert.Throws<AuthorizationException>( () => AuthTokenValidator.VerifyHeader( jwtHeader ) );
+		}
+
+		#endregion VerifyHeader Tests
+
+		#region Token Fetching Tests
+
+		[Test]
+		public void GetTokenFromCookie_AuthCookiePresent_CookieMatchesExpected() {
+
+			HttpRequest httpRequest = CreateHttpRequest();
+			httpRequest.Cookies.Add( new HttpCookie( "d2lApi", TOKEN_STUB ) );
+
+			string token = AuthTokenValidator.GetTokenFromCookie( httpRequest );
+			Assert.AreEqual( TOKEN_STUB, token );
+		}
+
+		[Test]
+		public void GetTokenFromCookie_AuthCookieNotPresent_NullReturned() {
+
+			HttpRequest httpRequest = CreateHttpRequest();
+
+			string token = AuthTokenValidator.GetTokenFromCookie( httpRequest );
+			Assert.Null( token );
+		}
+
+		#endregion GetTokenFromCookie Tests
+
+		#region GetTokenFromAuthHeader Tests
+
+		[Test]
+		public void GetTokenFromAuthHeader_AuthHeaderPresent_HeaderMatchesExpected() {
+
+			HttpRequest httpRequest = CreateHttpRequest();
+			AddAuthHeader( httpRequest, string.Format( "Bearer {0}", TOKEN_STUB ) );
+
+			string token = AuthTokenValidator.GetTokenFromAuthHeader( httpRequest );
+			Assert.AreEqual( TOKEN_STUB, token );
+		}
+
+		[Test]
+		public void GetTokenFromAuthHeader_AuthHeaderNotPresent_NullReturned() {
+
+			HttpRequest httpRequest = CreateHttpRequest();
+
+			string token = AuthTokenValidator.GetTokenFromAuthHeader( httpRequest );
+			Assert.Null( token );
+		}
+
+		[Test]
+		public void GetTokenFromAuthHeader_InvalidTokenPrefix_NullReturned() {
+
+			HttpRequest httpRequest = CreateHttpRequest();
+			AddAuthHeader( httpRequest, string.Format( "NOT_BEARER {0}", TOKEN_STUB ) );
+
+			string token = AuthTokenValidator.GetTokenFromAuthHeader( httpRequest );
+			Assert.Null( token );
+		}
+
+		#endregion GetTokenFromAuthHeader Tests
+
+		#region VerifyAndDecode Tests
+
+		[Test]
+		public void VerifyAndDecode_AuthTokenInBothHeaderAndCookie_ExpectException() {
+
+			HttpRequest httpRequest = CreateHttpRequest();
+			AddAuthHeader( httpRequest, string.Format( "Bearer {0}", TOKEN_STUB ) );
+			httpRequest.Cookies.Add( new HttpCookie( "d2lApi", TOKEN_STUB ) );
+
+			Assert.Throws<AuthorizationException>( () => m_authTokenValidator.VerifyAndDecode( httpRequest ) );
+		}
+
+		[Test]
+		public void VerifyAndDecode_ExpectException() {
+			Assert.Throws<AuthorizationException>( () => m_authTokenValidator.VerifyAndDecode( "AB.CD.EF.GH" ) );
+		}
+
+		[Test]
+		public void VerifyAndDecode_JwtHasFewerThanThreeSegments_ExpectException() {
+			Assert.Throws<AuthorizationException>( () => m_authTokenValidator.VerifyAndDecode( "AB.CD" ) );
+		}
+
+		[Test]
+		public void VerifyAndDecode_JwtIsEmpty_ExpectException() {
+			Assert.Throws<AuthorizationException>( () => m_authTokenValidator.VerifyAndDecode( string.Empty ) );
+		}
+
+		[Test]
+		public void VerifyAndDecode_JwtHasEmptySegment_ExpectException() {
+			Assert.Throws<AuthorizationException>( () => m_authTokenValidator.VerifyAndDecode( "AB..CD" ) );
+		}
+
+		#endregion VerifyAndDecode Tests
+
+		private string MakeJwtHeader( string algorithm, string type ) {
+			return string.Format( "{{ \"alg\": \"{0}\", \"typ\": \"{1}\"}}", algorithm, type );
+		}
+
+		private HttpRequest CreateHttpRequest() {
+			return new HttpRequest( null, "http://www.google.ca", null );
+		}
+
+		private void AddAuthHeader( HttpRequest httpRequest, string authHeaderValue ) {
+
+			// A hack for modifying http headers in an HttpRequest: http://stackoverflow.com/a/13307238
+			NameValueCollection headers = httpRequest.Headers;
+			Type headerCollectionType = headers.GetType();
+			ArrayList item = new ArrayList();
+
+			const BindingFlags flags = BindingFlags.InvokeMethod | BindingFlags.NonPublic | BindingFlags.Instance;
+
+			headerCollectionType.InvokeMember( "MakeReadWrite", flags, null, headers, null );
+			headerCollectionType.InvokeMember( "InvalidateCachedArrays", flags, null, headers, null );
+			item.Add( authHeaderValue );
+			headerCollectionType.InvokeMember( "BaseAdd", flags, null, headers, new object[] { "Authorization", item } );
+			headerCollectionType.InvokeMember( "MakeReadOnly", flags, null, headers, null );
 		}
 	}
 }
