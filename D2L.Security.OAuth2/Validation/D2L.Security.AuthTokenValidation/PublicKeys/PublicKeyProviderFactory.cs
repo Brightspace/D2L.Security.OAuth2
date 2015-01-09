0eb9c854ab5cb914853dc71b895add011b95de1f
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-09 18:06:00 -0500

    Clean up key provider.

--- D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/PublicKeys/PublicKeyProviderFactory.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/PublicKeys/PublicKeyProviderFactory.cs
@@ -3,7 +3,7 @@
 namespace D2L.Security.AuthTokenValidation.PublicKeys {
 	internal static class PublicKeyProviderFactory {
 
-		private const string AUTHORITY = "";
+		private const string AUTHORITY = "https://phwinsl01.proddev.d2l:44333/core/";
 		private static readonly IPublicKeyProvider Instance = new PublicKeyProvider( AUTHORITY );
 
 		internal static IPublicKeyProvider Create() {
