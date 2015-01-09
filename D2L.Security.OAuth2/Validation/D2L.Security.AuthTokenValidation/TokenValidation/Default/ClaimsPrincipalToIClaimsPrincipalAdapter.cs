68fea3cec2902166e60223259950c81398b501a5
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-09 16:48:39 -0500

    Validation works well. I've begun refactoring the code.

---
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/TokenValidation/Default/ClaimsPrincipalToIClaimsPrincipalAdapter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace D2L.Security.AuthTokenValidation.TokenValidation.Default {
+	internal sealed class ClaimsPrincipalToIClaimsPrincipalAdapter : IClaimsPrincipal {
+		
+		private readonly ClaimsPrincipal m_inner;
+		
+		public ClaimsPrincipalToIClaimsPrincipalAdapter( ClaimsPrincipal inner ) {
+			m_inner = inner;
+		}
+
+		IIdentity IPrincipal.Identity {
+			get { return m_inner.Identity; }
+		}
+
+		bool IPrincipal.IsInRole( string role ) {
+			return m_inner.IsInRole( role );
+		}
+
+		IEnumerable<Claim> IClaimsPrincipal.Claims {
+			get { return m_inner.Claims; }
+		}
+	}
+}
---
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/TokenValidation/IJWTValidator.cs
@@ -0,0 +1,6 @@
+﻿namespace D2L.Security.AuthTokenValidation.TokenValidation {
+	
+	interface IJWTValidator {
+		bool TryValidate( string jwt, out IClaimsPrincipal claimsPrincipal );
+	}
+}
