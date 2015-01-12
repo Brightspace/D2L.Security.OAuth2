d112379450c0cc8bcc4006efc9511191836fe3f4
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-12 14:03:15 -0500

    No more singletons for now.

--- D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/PublicKeys/PublicKeyProviderFactory.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/PublicKeys/PublicKeyProviderFactory.cs
@@ -5,19 +5,10 @@ using D2L.Security.AuthTokenValidation.PublicKeys.OpenIdConfigurations.Default;
 namespace D2L.Security.AuthTokenValidation.PublicKeys {
 	internal static class PublicKeyProviderFactory {
 
-		private static object Lock = new object();
-		private static IPublicKeyProvider Instance;
-
 		internal static IPublicKeyProvider Create( string authority ) {
-			if( Instance == null ) {
-				lock( Lock ) {
-					if( Instance == null ) {
-						IOpenIdConfigurationFetcher openIdFetcher = new OpenIdConfigurationFetcher( authority );
-						Instance = new PublicKeyProvider( openIdFetcher );
-					}
-				}
-			}
-			return Instance;
+			IOpenIdConfigurationFetcher openIdFetcher = new OpenIdConfigurationFetcher( authority );
+			IPublicKeyProvider provider = new PublicKeyProvider( openIdFetcher );
+			return provider;
 		}
 	}
 }
