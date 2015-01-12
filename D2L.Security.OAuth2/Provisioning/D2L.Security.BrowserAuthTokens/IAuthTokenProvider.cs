09d0d4eb7b1e4fd25833f7079d507188095e2611
Jacob Parker <Jacob.Parker@D2L.com>
2015-01-12 06:50:48 -0500

    Preparing for nuget

---
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.BrowserAuthTokens/IAuthTokenProvider.cs
@@ -0,0 +1,5 @@
+﻿namespace D2L.Security.BrowserAuthTokens {
+	public interface IAuthTokenProvider {
+		string GetTokenForUser( string tenantId, long userId, string xsrfToken, long duration );
+	}
+}
