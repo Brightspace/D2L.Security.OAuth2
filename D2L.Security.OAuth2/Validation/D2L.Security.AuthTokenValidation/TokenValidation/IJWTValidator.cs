648a1901fc3f540513c61eb4965f1e35e851471c
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-12 15:10:23 -0500

    Simplify validator and no longer catch exceptions.

--- D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/TokenValidation/IJWTValidator.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/TokenValidation/IJWTValidator.cs
@@ -4,13 +4,6 @@
 	/// Entry point into validating a JWT
 	/// </summary>
 	interface IJWTValidator {
-
-		/// <summary>
-		/// Does not throw exceptions.
-		/// </summary>
-		/// <param name="jwt">The JWT to validate</param>
-		/// <param name="claimsPrincipal">The result of a successful validation</param>
-		/// <returns>True if the validation was successful, and False otherwise</returns>
-		bool TryValidate( string jwt, out IClaimsPrincipal claimsPrincipal );
+		IClaimsPrincipal Validate( string jwt );
 	}
 }
