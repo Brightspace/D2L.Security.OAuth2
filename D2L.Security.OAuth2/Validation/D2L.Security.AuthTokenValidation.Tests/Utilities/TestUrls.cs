08cc69c13ff7426886841aa5e44fd9259d50b71f
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-13 17:37:56 -0500

    Kept change to const.

--- D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation.Tests/Utilities/TestUrls.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation.Tests/Utilities/TestUrls.cs
@@ -1,7 +1,7 @@
 ﻿namespace D2L.Security.AuthTokenValidation.Tests.Utilities {
 	internal static class TestUrls {
 
-		private static readonly string AUTH_SERVER_SITE = "https://phwinsl01.proddev.d2l:44333";
+		private const string AUTH_SERVER_SITE = "https://phwinsl01.proddev.d2l:44333";
 
 		internal static readonly string AUTH_TOKEN_PROVISIONING_URL = AUTH_SERVER_SITE + "/core/connect/token";
 		internal static readonly string TOKEN_VERIFICATION_AUTHORITY_URL = AUTH_SERVER_SITE + "/core/";
