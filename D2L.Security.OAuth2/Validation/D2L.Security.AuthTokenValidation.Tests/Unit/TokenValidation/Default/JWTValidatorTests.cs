491f17dd6f3c080d7d9c405cc22b2a513c27231c
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-13 09:34:58 -0500

    JWTValidator tests skeleton.

---
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation.Tests/Unit/TokenValidation/Default/JWTValidatorTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace D2L.Security.AuthTokenValidation.Tests.Unit.TokenValidation.Default {
+	
+	[TestFixture]
+	internal sealed class JWTValidatorTests {
+
+		[Test]
+		public void RSA_ValidIssuer_NonExpiredToken_Success() {
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void RSA_ValidIssuer_ExpiredToken_Failure() {
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void RSA_InvalidIssuer_NonExpiredToken_Failure() {
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void NonRSA_ValidIssuer_NonExpiredToken_Failure() {
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void NonRSA_InvalidIssuer_NonExpiredToken_Failure() {
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void RSA_InvalidIssuer_ExpiredToken_Failure() {
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void NonRSA_InvalidIssuer_ExpiredToken_Failure() {
+			Assert.Inconclusive();
+		}
+	}
+}
