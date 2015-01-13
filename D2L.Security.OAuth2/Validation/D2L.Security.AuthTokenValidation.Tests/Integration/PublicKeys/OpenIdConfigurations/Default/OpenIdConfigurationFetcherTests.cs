b3cb5060ca99a39e86dcdf73fa07e883e590a26c
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-13 10:33:14 -0500

    More test fixtures.

---
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation.Tests/Integration/PublicKeys/OpenIdConfigurations/Default/OpenIdConfigurationFetcherTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace D2L.Security.AuthTokenValidation.Tests.Integration.PublicKeys.OpenIdConfigurations.Default {
+	
+	[TestFixture]
+	internal sealed class OpenIdConfigurationFetcherTests {
+
+		[Test]
+		public void Fetch_Success() {
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void Fetch_InvalidUrl_Throws() {
+			Assert.Inconclusive();
+		}
+	}
+}
---
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation.Tests/Unit/PublicKeys/Default/PublicKeyProviderTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace D2L.Security.AuthTokenValidation.Tests.Unit.PublicKeys.Default {
+	
+	[TestFixture]
+	internal sealed class PublicKeyProviderTests {
+
+		[Test]
+		public void Get_Success() {
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void Get_WrongJsonWebKeyCount_Throws() {
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void Get_InvalidKeyType_Throws() {
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void Get_WrongX5CEntryCount_Throws() {
+			Assert.Inconclusive();
+		}
+	}
+}
