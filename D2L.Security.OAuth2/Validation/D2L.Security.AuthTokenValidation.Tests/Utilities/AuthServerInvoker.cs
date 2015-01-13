0ca87517eec1567cde5afa62e684e3f3c2a0d4f6
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-13 16:20:01 -0500

    Cleanup around full-stack test.

--- D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation.Tests/Utilities/AuthServerInvoker.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation.Tests/Utilities/AuthServerInvoker.cs
@@ -6,52 +6,43 @@ using System.Web;
 
 namespace D2L.Security.AuthTokenValidation.Tests.Utilities {
 
-	internal static class LOReSScopes {
-		internal static readonly string MANAGE = "https://api.brightspace.com/auth/lores.manage";
-	}
-
-	internal static class LOReSManager {
-		internal static readonly string CLIENT_ID = "lores_manager_client";
-		internal static readonly string SECRET = "lores_manager_secret";
-	}
-
 	internal static class AuthServerInvoker {
 
-		internal static readonly string AUTH_SERVER = "https://phwinsl01.proddev.d2l:44333/core/";
 		private static readonly string AUTH_TOKEN_PROVISIONING_URL = "https://phwinsl01.proddev.d2l:44333/core/connect/token";
+
+		internal static readonly string AUTHORITY_URL = "https://phwinsl01.proddev.d2l:44333/core/";
 		
 		public static string AuthenticateAndGetJWT( string clientId, string clientSecret, string scope ) {
-			string authenticateHeaderValue = HttpUtility.UrlEncode( clientId ) + ":" + HttpUtility.UrlEncode( clientSecret );
-			authenticateHeaderValue = authenticateHeaderValue.ToBase64();
-			authenticateHeaderValue = "Basic " + authenticateHeaderValue;
-
-			string formContents = "grant_type=client_credentials&scope=" + scope;
-
-			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(AUTH_TOKEN_PROVISIONING_URL);
+			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create( AUTH_TOKEN_PROVISIONING_URL );
 			request.Method = "POST";
 			request.ContentType = "application/x-www-form-urlencoded";
-			request.Headers["Authorization"] = authenticateHeaderValue;
 
+			string authorizationHeaderValue = HttpUtility.UrlEncode( clientId ) + ":" + HttpUtility.UrlEncode( clientSecret );
+			authorizationHeaderValue = authorizationHeaderValue.ToBase64();
+			authorizationHeaderValue = "Basic " + authorizationHeaderValue;
+			request.Headers["Authorization"] = authorizationHeaderValue;
+
+			string formContents = "grant_type=client_credentials&scope=" + scope;
 			using( StreamWriter write = new StreamWriter( request.GetRequestStream() ) ) {
 				write.Write( formContents );
 			}
 
-			HttpWebResponse response = request.GetResponse() as HttpWebResponse; // Exception here
+			WebResponse response = request.GetResponse();
 
 			DataContractJsonSerializer serializer = new DataContractJsonSerializer( typeof( AuthServerResponse ) );
 			AuthServerResponse authServerResponse = (AuthServerResponse)serializer.ReadObject( response.GetResponseStream() );
 
 			return authServerResponse.access_token;
 		}
-	}
 
-	[DataContract]
-	public sealed class AuthServerResponse {
-		[DataMember]
-		public string access_token { get; set; }
-		[DataMember]
-		public string token_type { get; set; }
-		[DataMember]
-		public string expires_in { get; set; }
+		[DataContract]
+		internal sealed class AuthServerResponse {
+			[DataMember]
+			public string access_token { get; set; }
+			[DataMember]
+			public string token_type { get; set; }
+			[DataMember]
+			public string expires_in { get; set; }
+		}
 	}
 }
