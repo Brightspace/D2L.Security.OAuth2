dd051888a937c743c3e0638d7e6b0b6c07fe9fcd
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-15 13:33:42 -0500

    Validator tests.

---
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/TokenValidation/Exceptions/InvalidTokenTypeException.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace D2L.Security.AuthTokenValidation.TokenValidation.Exceptions {
+	internal sealed class InvalidTokenTypeException : Exception {
+		internal InvalidTokenTypeException( string message ) : base( message ) { }
+	}
+}
