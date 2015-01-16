bdce9bafed2a642e8aa8dea0317817e3c85504c1
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-16 12:03:27 -0500

    Refactor test.

---
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/IDisposableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace D2L.Security.AuthTokenValidation {
+	internal static class IDisposableExtensions {
+
+		internal static void SafeDispose( this IDisposable me ) {
+			if( me == null ){
+				return;
+			}
+			try {
+				me.Dispose();
+			} catch {}
+		}
+	}
+}
