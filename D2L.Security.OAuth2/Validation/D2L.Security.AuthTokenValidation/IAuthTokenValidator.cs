50d44579bf626614c293104c0bfc09d96cad67bb
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-21 17:20:52 -0500

    Better language

--- D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/IAuthTokenValidator.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/IAuthTokenValidator.cs
@@ -8,7 +8,7 @@ namespace D2L.Security.AuthTokenValidation {
 		/// Verify and decode a token extracted from a request
 		/// 
 		/// Throws D2L.Security.AuthTokenValidation.TokenExpiredException if the token is expired.
-		/// Throws D2L.Security.AuthTokenValidation.AuthorizationException if the validation failed for all other reasons
+		/// Throws D2L.Security.AuthTokenValidation.AuthorizationException if the validation failed for any other reason
 		/// </summary>
 		/// <param name="request">The request whose token to validate and decode</param>
 		/// <returns>A container of properties</returns>
@@ -18,7 +18,7 @@ namespace D2L.Security.AuthTokenValidation {
 		/// Verify and decode a token
 		/// 
 		/// Throws D2L.Security.AuthTokenValidation.TokenExpiredException if the token is expired.
-		/// Throws D2L.Security.AuthTokenValidation.AuthorizationException if the validation failed for all other reasons
+		/// Throws D2L.Security.AuthTokenValidation.AuthorizationException if the validation failed for any other reason
 		/// </summary>
 		/// <param name="jwt">The jwt to validate and decode</param>
 		/// <returns>A container of properties</returns>
