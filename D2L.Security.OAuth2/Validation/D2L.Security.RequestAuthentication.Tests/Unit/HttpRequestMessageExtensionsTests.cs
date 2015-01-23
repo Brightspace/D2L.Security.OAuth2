728d01f84b1494fc01e680db10c123cc4458e803
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-23 13:44:21 -0500

    Cookie extraction tests

--- D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication.Tests/Unit/HttpRequestMessageExtensionsTests.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication.Tests/Unit/HttpRequestMessageExtensionsTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net.Http;
 using NUnit.Framework;
 
 namespace D2L.Security.RequestAuthentication.Tests.Unit {
@@ -10,9 +6,43 @@ namespace D2L.Security.RequestAuthentication.Tests.Unit {
 	[TestFixture]
 	internal class HttpRequestMessageExtensionsTests {
 
+		private const string COOKIE_HEADER = "Cookie";
+
 		[Test]
-		public void GetCookieValue_Null_ExpectNull() {
+		public void GetCookieValue_SingleCookieValue_Success() {
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void GetCookieValue_MultipleCookieValues_Success() {
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void GetCookieValue_NullRequest_ExpectNull() {
 			Assert.IsNull( HttpRequestMessageExtensions.GetCookieValue( null, "dummycookiename" ) );
+		}
+
+		[Test]
+		public void GetCookieValue_NullCookieName_ExpectNull() {
+			Assert.IsNull( new HttpRequestMessage().GetCookieValue( null ) );
+		}
+
+		[Test]
+		public void GetCookieValue_EmptyCookieName_ExpectNull() {
+			Assert.IsNull( new HttpRequestMessage().GetCookieValue( string.Empty ) );
+		}
+
+		[Test]
+		public void GetCookieValue_NoCookieHeader_ExpectNull() {
+			Assert.IsNull( new HttpRequestMessage().GetCookieValue( "dummycookiename" ) );
+		}
+
+		[Test]
+		public void GetCookieValue_NoCookies_ExpectNull() {
+			HttpRequestMessage request = new HttpRequestMessage();
+			request.Headers.Add( COOKIE_HEADER, new string[] { } );
+			Assert.IsNull( request.GetCookieValue( "dummycookiename" ) );
 		}
 	}
 }
