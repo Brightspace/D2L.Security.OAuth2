22a18c59366fa55096ec7ebbbbfc7cf94f9b4364
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-23 15:13:44 -0500

    Factor out extractions and add test fixtures

---
+++ D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication/HttpRequestExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace D2L.Security.RequestAuthentication {
+	internal static class HttpRequestExtensions {
+
+		private const string XSRF_HEADER = "X-Csrf-Token";
+		private const string BEARER_TOKEN_AUTHORIZATION_SCHEME = "Bearer";
+		private const string BEARER_TOKEN_AUTHORIZATION_SCHEME_PREFIX =
+			BEARER_TOKEN_AUTHORIZATION_SCHEME + " ";
+		private const string AUTHORIZATION_HEADER_NAME = "Authorization";
+
+		/// <summary>
+		/// Return the value of a cookie
+		/// </summary>
+		/// <param name="request">The request</param>
+		/// <param name="cookieName">The name of the cookie</param>
+		/// <returns>A cookie value, or null if the specified cookie was not found</returns>
+		internal static string GetCookieValue( this HttpRequest request, string cookieName ) {
+			if( string.IsNullOrEmpty( cookieName ) ) {
+				return null;
+			}
+
+			if( request == null ) {
+				return null;
+			}
+
+			HttpCookie cookie = request.Cookies.Get( cookieName );
+			if( cookie == null ) {
+				return null;
+			}
+
+			return cookie.Value;
+		}
+
+		/// <summary>
+		/// Returns the value of the bearer token.
+		/// </summary>
+		/// <param name="request">The request</param>
+		/// <returns>The value of the bearer token, or null if the bearer token is not set</returns>
+		internal static string GetBearerTokenValue( this HttpRequest request ) {
+			if( request == null ) {
+				return null;
+			}
+
+			string headerValue = request.Headers[AUTHORIZATION_HEADER_NAME];
+			if( headerValue == null ) {
+				return null;
+			}
+
+			if( !headerValue.StartsWith( BEARER_TOKEN_AUTHORIZATION_SCHEME_PREFIX ) ) {
+				return null;
+			}
+
+			string bearerToken = headerValue.Substring( BEARER_TOKEN_AUTHORIZATION_SCHEME_PREFIX.Length );
+			return bearerToken;
+		}
+
+		/// <summary>
+		/// Returns the value of the Xsrf header.
+		/// </summary>
+		/// <param name="request">The request</param>
+		/// <returns>The value of the Xsrf header, or null if the Xsrf header was not found</returns>
+		internal static string GetXsrfValue( this HttpRequest request ) {
+			string xsrfValue = request.Headers[XSRF_HEADER];
+			return xsrfValue;
+		}
+	}
+}
