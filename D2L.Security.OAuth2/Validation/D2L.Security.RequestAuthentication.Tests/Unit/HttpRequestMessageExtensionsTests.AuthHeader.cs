e02bf4f71bdc0427210f452dabcac0e25ea4b8da
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-23 14:36:39 -0500

    Bearer token test fixture

---
+++ D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication.Tests/Unit/HttpRequestMessageExtensionsTests.AuthHeader.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace D2L.Security.RequestAuthentication.Tests.Unit {
+	
+	[TestFixture]
+	internal partial class HttpRequestMessageExtensionsTests {
+
+		private const string BEARER_TOKEN_AUTHORIZATION_SCHEME = "Bearer";
+
+		[Test]
+		public void GetBearerTokenValue_Success() {
+			Assert.Inconclusive();
+		}
+		
+		[Test]
+		public void GetBearerTokenValue_NullRequest_ExpectNull() {
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void GetBearerTokenValue_NoAuthorizationHeader_ExpectNull() {
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void GetBearerTokenValue_WrongScheme_ExpectNull() {
+			Assert.Inconclusive();
+		}
+	}
+}
