e5c7cfc3903278a484aff0112a745822c7758bbf
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-23 16:16:17 -0500

    Factor out core authentication

--- D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication/Default/RequestAuthenticator.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication/Default/RequestAuthenticator.cs
@@ -1,16 +1,16 @@
 ﻿using System.Net.Http;
 using System.Web;
-using D2L.Security.AuthTokenValidation;
+using D2L.Security.RequestAuthentication.Core;
 
 namespace D2L.Security.RequestAuthentication.Default {
 	internal sealed class RequestAuthenticator : IRequestAuthenticator {
 
 		private const string COOKIE_NAME = "d2lApi";
 
-		private readonly IAuthTokenValidator m_tokenValidator;
+		private readonly ICoreAuthenticator m_coreAuthenticator;
 
-		internal RequestAuthenticator( IAuthTokenValidator tokenValidator ) {
-			m_tokenValidator = tokenValidator;
+		internal RequestAuthenticator( ICoreAuthenticator coreAuthenticator ) {
+			m_coreAuthenticator = coreAuthenticator;
 		}
 
 		AuthenticationResult IRequestAuthenticator.AuthenticateAndExtract( HttpRequestMessage request, out ID2LPrincipal principal ) {
@@ -18,7 +18,7 @@ namespace D2L.Security.RequestAuthentication.Default {
 			string bearerToken = request.GetBearerTokenValue();
 			string xsrfToken = request.GetXsrfValue();
 
-			return TryAuthenticate( cookie, xsrfToken, bearerToken, out principal );
+			return m_coreAuthenticator.Authenticate( cookie, xsrfToken, bearerToken, out principal );
 		}
 
 		AuthenticationResult IRequestAuthenticator.AuthenticateAndExtract( HttpRequest request, out ID2LPrincipal principal ) {
@@ -26,50 +26,7 @@ namespace D2L.Security.RequestAuthentication.Default {
 			string bearerToken = request.GetBearerTokenValue();
 			string xsrfToken = request.GetXsrfValue();
 
-			return TryAuthenticate( cookie, xsrfToken, bearerToken, out principal );
-		}
-
-		private AuthenticationResult TryAuthenticate( 
-			string cookie, 
-			string xsrfHeader, 
-			string bearerToken, 
-			out ID2LPrincipal principal 
-			) {
-
-			if( cookie == null && bearerToken == null ) {
-				principal = null;
-				return AuthenticationResult.Anonymous;
-			}
-
-			if( cookie != null && bearerToken != null ) {
-				principal = null;
-				return AuthenticationResult.TokenLocationConflict;
-			}
-
-			bool isBrowserUser = cookie != null;
-
-			string token = bearerToken ?? cookie;
-
-			IGenericPrincipal claims;
-			ValidationResult validationResult = m_tokenValidator.VerifyAndDecode( token, out claims );
-
-			if( validationResult == ValidationResult.TokenExpired ) {
-				principal = null;
-				return AuthenticationResult.Expired;
-			}
-
-			bool xsrfSafe = false;
-			if( isBrowserUser && xsrfHeader != null ) {
-				if( claims.XsrfToken != xsrfHeader ) {
-					principal = null;
-					return AuthenticationResult.BadXsrf;
-				}
-
-				xsrfSafe = true;
-			}
-
-			principal = new D2LPrincipal( xsrfSafe );
-			return AuthenticationResult.Success;
+			return m_coreAuthenticator.Authenticate( cookie, xsrfToken, bearerToken, out principal );
 		}
 	}
 }
