1d7c1a6864df112f5ad6023268352d398b739a6d
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-23 13:25:11 -0500

    Factor out cookie extraction

---
+++ D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication.Tests/Unit/HttpRequestMessageExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace D2L.Security.RequestAuthentication.Tests.Unit {
+	
+	[TestFixture]
+	internal class HttpRequestMessageExtensionsTests {
+
+		[Test]
+		public void GetCookieValue_Null_ExpectNull() {
+			Assert.IsNull( HttpRequestMessageExtensions.GetCookieValue( null, "dummycookiename" ) );
+		}
+	}
+}
