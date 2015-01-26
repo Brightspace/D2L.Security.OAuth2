3b198ec8746373f3734087d4b4bfc0a8fc52f953
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-26 16:10:05 -0500

    Request component extraction tests

---
+++ D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication.Tests/Integration/Default/RequestAuthenticatorTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Net.Http;
+using System.Web;
+using D2L.Security.AuthTokenValidation;
+using D2L.Security.RequestAuthentication.Core;
+using D2L.Security.RequestAuthentication.Default;
+using D2L.Security.RequestAuthentication.Tests.Utilities;
+using Moq;
+using NUnit.Framework;
+
+namespace D2L.Security.RequestAuthentication.Tests.Integration.Default {
+	
+	[TestFixture]
+	internal sealed class RequestAuthenticatorTests {
+
+		[Test]
+		public void AuthenticateAndExtract_HttpRequestMessage_ExtractsComponentsProperly() {
+			string cookieValue = "dummyjwt";
+			string xsrfValue = "dummyxsrf";
+			string bearerTokenValue = "dummybearertoken";
+
+			HttpRequestMessage httpRequestMessage = new HttpRequestMessage();
+			RequestBuilder.AddAuthHeader( httpRequestMessage, bearerTokenValue );
+			RequestBuilder.AddXsrfHeader( httpRequestMessage, xsrfValue );
+			RequestBuilder.AddCookie( httpRequestMessage, cookieValue );
+
+			IGenericPrincipal claims = new Mock<IGenericPrincipal>().Object;
+			Mock<IAuthTokenValidator> validatorMock = new Mock<IAuthTokenValidator>();
+			validatorMock.Setup( x => x.VerifyAndDecode( It.IsAny<string>(), out claims ) )
+				.Returns( ValidationResult.Success );
+
+			Mock<ICoreAuthenticator> coreAuthenticatorMock = new Mock<ICoreAuthenticator>();
+			IRequestAuthenticator authenticator = new RequestAuthenticator( coreAuthenticatorMock.Object );
+
+			ID2LPrincipal principal;
+			AuthenticationResult result = authenticator.AuthenticateAndExtract( httpRequestMessage, out principal );
+
+			Assert.AreEqual( AuthenticationResult.Success, result );
+			coreAuthenticatorMock.Verify( x => x.Authenticate(
+				cookieValue,
+				xsrfValue,
+				bearerTokenValue,
+				out principal
+				), Times.Once
+				);
+		}
+
+		[Test]
+		public void AuthenticateAndExtract_HttpRequest_ExtractsComponentsProperly() {
+			string cookieValue = "dummyjwt";
+			string xsrfValue = "dummyxsrf";
+			string bearerTokenValue = "dummybearertoken";
+
+			HttpRequest httpRequest = new HttpRequest( null, "http://d2l.com", null );
+			RequestBuilder.AddCookie( httpRequest, cookieValue );
+			RequestBuilder.AddAuthHeader( httpRequest, "Bearer " + bearerTokenValue );
+			RequestBuilder.AddXsrfHeader( httpRequest, xsrfValue );
+
+			IGenericPrincipal claims = new Mock<IGenericPrincipal>().Object;
+			Mock<IAuthTokenValidator> validatorMock = new Mock<IAuthTokenValidator>();
+			validatorMock.Setup( x => x.VerifyAndDecode( It.IsAny<string>(), out claims ) )
+				.Returns( ValidationResult.Success );
+
+			Mock<ICoreAuthenticator> coreAuthenticatorMock = new Mock<ICoreAuthenticator>();
+			IRequestAuthenticator authenticator = new RequestAuthenticator( coreAuthenticatorMock.Object );
+
+			ID2LPrincipal principal;
+			AuthenticationResult result = authenticator.AuthenticateAndExtract( httpRequest, out principal );
+
+			Assert.AreEqual( AuthenticationResult.Success, result );
+			coreAuthenticatorMock.Verify( x => x.Authenticate(
+				cookieValue,
+				xsrfValue,
+				bearerTokenValue,
+				out principal
+				), Times.Once
+				);
+		}
+	}
+}
---
+++ D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication.Tests/Utilities/RequestBuilder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Web;
+
+namespace D2L.Security.RequestAuthentication.Tests.Utilities {
+	internal static class RequestBuilder {
+
+		private const string COOKIE_NAME = "d2lApi";
+		private const string COOKIE_HEADER_NAME = "Cookie";
+		private const string AUTH_HEADER_SCHEME = "Bearer";
+		private const string BEARER_TOKEN_HEADER_NAME = "Authorization";
+		private const string XSRF_HEADER_NAME = "X-Csrf-Token";
+
+		internal static void AddAuthHeader( HttpRequest httpRequest, string authHeaderValue ) {
+			AddHeader( httpRequest, BEARER_TOKEN_HEADER_NAME, authHeaderValue );
+		}
+
+		internal static void AddXsrfHeader( HttpRequest httpRequest, string xsrfHeaderValue ) {
+			AddHeader( httpRequest, XSRF_HEADER_NAME, xsrfHeaderValue );
+		}
+
+		internal static void AddCookie( HttpRequest httpRequest, string cookieValue ) {
+			httpRequest.Cookies.Add( new HttpCookie( COOKIE_NAME, cookieValue ) );
+		}
+
+		internal static void AddAuthHeader( HttpRequestMessage httpRequestMessage, string authHeaderValue ) {
+			AuthenticationHeaderValue authHeaderVal = new AuthenticationHeaderValue( AUTH_HEADER_SCHEME, authHeaderValue );
+			httpRequestMessage.Headers.Authorization = authHeaderVal;
+		}
+
+		internal static void AddXsrfHeader( HttpRequestMessage httpRequestMessage, string xsrfHeaderValue ) {
+			httpRequestMessage.Headers.Add( XSRF_HEADER_NAME, xsrfHeaderValue );
+		}
+
+		internal static void AddCookie( HttpRequestMessage httpRequestMessage, string cookieValue ) {
+			string cookieHeaderValue = COOKIE_NAME + "=" + cookieValue;
+			httpRequestMessage.Headers.Add( COOKIE_HEADER_NAME, cookieHeaderValue );
+		}
+
+		private static void AddHeader( HttpRequest httpRequest, string headerName, string headerValue ) {
+
+			// A hack for modifying http headers in an HttpRequest: http://stackoverflow.com/a/13307238
+			NameValueCollection headers = httpRequest.Headers;
+			Type headerCollectionType = headers.GetType();
+			ArrayList item = new ArrayList();
+
+			const BindingFlags flags = BindingFlags.InvokeMethod | BindingFlags.NonPublic | BindingFlags.Instance;
+
+			headerCollectionType.InvokeMember( "MakeReadWrite", flags, null, headers, null );
+			headerCollectionType.InvokeMember( "InvalidateCachedArrays", flags, null, headers, null );
+			item.Add( headerValue );
+			headerCollectionType.InvokeMember( "BaseAdd", flags, null, headers, new object[] { headerName, item } );
+			headerCollectionType.InvokeMember( "MakeReadOnly", flags, null, headers, null );
+		}
+	}
+}
