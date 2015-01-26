f031c9e97911033766b7860ef8f39fd870cfd7fa
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-26 17:39:17 -0500

    Adding ful stack tests fixture

---
+++ D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication.Tests/Integration/FullStack/FullStackTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http;
+using D2L.Security.RequestAuthentication.Tests.Utilities;
+using NUnit.Framework;
+
+namespace D2L.Security.RequestAuthentication.Tests.Integration.FullStack {
+	
+	[TestFixture]
+	internal sealed class FullStackTests {
+
+		[Test]
+		public void IRequestAuthenticator_AuthenticateAndExtract_NoXsrf_HttpRequestMessage_Success() {
+			string cookieValue = TestTokens.VALID_NO_XSRF_JWT;
+
+			HttpRequestMessage httpRequestMessage = new HttpRequestMessage();
+			RequestBuilder.AddCookie( httpRequestMessage, cookieValue );
+
+			IRequestAuthenticator requestAuthenticator = RequestAuthenticatorFactory.Create(
+				TestUris.TOKEN_VERIFICATION_AUTHORITY_URI,
+				Mode.SkipXsrfValidation
+				);
+
+			ID2LPrincipal principal;
+			AuthenticationResult result = requestAuthenticator.AuthenticateAndExtract( httpRequestMessage, out principal );
+		}
+
+		[Test]
+		public void IRequestAuthenticator_AuthenticateAndExtract_WithXsrf_HttpRequestMessage_Success() {
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void IRequestAuthenticator_AuthenticateAndExtract_NoXsrf_HttpRequest_Success() {
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void IRequestAuthenticator_AuthenticateAndExtract_WithXsrf_HttpRequest_Success() {
+			Assert.Inconclusive();
+		}
+	}
+}
