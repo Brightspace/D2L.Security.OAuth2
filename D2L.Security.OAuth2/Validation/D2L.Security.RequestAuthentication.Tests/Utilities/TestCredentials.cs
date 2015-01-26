b3434abb97f7d24472e41a18e934ea36cfd8bbd9
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-26 17:39:17 -0500

    Adding ful stack tests fixture

---
+++ D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication.Tests/Utilities/TestCredentials.cs
@@ -0,0 +1,9 @@
+﻿namespace D2L.Security.RequestAuthentication.Tests.Utilities {
+
+	internal static class TestCredentials {
+
+		internal static class LOReSScopes {
+			internal const string MANAGE = "https://api.brightspace.com/auth/lores.manage";
+		}
+	}
+}
