7097e856433946ae6baea1978d1944e4a579c430
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-26 13:09:03 -0500

    xsrf validation

--- D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication/RequestAuthenticatorFactory.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication/RequestAuthenticatorFactory.cs
@@ -6,14 +6,9 @@ using D2L.Security.RequestAuthentication.Default;
 namespace D2L.Security.RequestAuthentication {
 	public static class RequestAuthenticatorFactory {
 
-		public static IRequestAuthenticator Create( IAuthTokenValidator tokenValidator ) {
-			ICoreAuthenticator coreAuthenticator = CoreAuthenticatorFactory.Create( tokenValidator );
-			return new RequestAuthenticator( coreAuthenticator );
-		}
-
-		public static IRequestAuthenticator Create( Uri authServiceEndpoint ) {
+		public static IRequestAuthenticator Create( Uri authServiceEndpoint, bool mustValidateXsrf ) {
 			IAuthTokenValidator tokenValidator = AuthTokenValidatorFactory.Create( authServiceEndpoint );
-			ICoreAuthenticator coreAuthenticator = CoreAuthenticatorFactory.Create( tokenValidator );
+			ICoreAuthenticator coreAuthenticator = CoreAuthenticatorFactory.Create( tokenValidator, mustValidateXsrf );
 			return new RequestAuthenticator( coreAuthenticator );
 		}
 	}
