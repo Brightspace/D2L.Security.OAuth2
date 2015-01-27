a1509d2cbe01607694cabd518d9f9afa4dee62d1
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-27 14:48:49 -0500

    Nuget

--- D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/2publishNugetPackage.bat
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/2publishNugetPackage.bat
@@ -1,3 +1,3 @@
-..\.nuget\nuget push D2L.Security.AuthTokenValidation.1.0.8.0.nupkg 6E553704-38F4-4543-85EF-D3D6D992A8A0 -s http://nuget.build.d2l/nuget/stable
+..\.nuget\nuget push D2L.Security.AuthTokenValidation.1.0.8.1.nupkg 6E553704-38F4-4543-85EF-D3D6D992A8A0 -s http://nuget.build.d2l/nuget/stable
 
 pause
--- D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/Properties/AssemblyInfo.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/Properties/AssemblyInfo.cs
@@ -13,8 +13,8 @@
 [assembly: AssemblyProduct( "Brightspace" )]
 
 // Nuget: Version
-[assembly: AssemblyInformationalVersion( "1.0.8.0" )]
-[assembly: AssemblyVersion( "1.0.8.0" )]
-[assembly: AssemblyFileVersion( "1.0.8.0" )]
+[assembly: AssemblyInformationalVersion( "1.0.8.1" )]
+[assembly: AssemblyVersion( "1.0.8.1" )]
+[assembly: AssemblyFileVersion( "1.0.8.1" )]
 
 [assembly: AssemblyCopyright( "Copyright © Desire2Learn" )]
