f61fc3487442f40925a8c2226efa14b15380197c
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-27 11:23:02 -0500

    Expose documentation and suppress warnings on missing documentation on publics

---
+++ D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication.Tests/Integration/FullStack/FullStackTests.HttpRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+using D2L.Security.RequestAuthentication.Tests.Utilities;
+using NUnit.Framework;
+
+namespace D2L.Security.RequestAuthentication.Tests.Integration.FullStack {
+	
+	[TestFixture]
+	internal sealed partial class FullStackTests {
+
+		// This will mirror tests for the HttpRequestMessage overload
+	}
+}
