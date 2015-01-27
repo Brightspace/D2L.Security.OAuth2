5edf5ea571cc1aaa4d28b36ac97377022665a21b
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-27 16:14:37 -0500

    Nuget

--- D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication/2publishNugetPackage.bat
+++ D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication/2publishNugetPackage.bat
@@ -1,4 +1,4 @@
 
-..\.nuget\nuget push D2L.Security.RequestAuthentication.1.0.1.0.nupkg 6E553704-38F4-4543-85EF-D3D6D992A8A0 -s http://nuget.build.d2l/nuget/stable
+..\.nuget\nuget push D2L.Security.RequestAuthentication.1.0.1.1.nupkg 6E553704-38F4-4543-85EF-D3D6D992A8A0 -s http://nuget.build.d2l/nuget/stable
 
 pause
