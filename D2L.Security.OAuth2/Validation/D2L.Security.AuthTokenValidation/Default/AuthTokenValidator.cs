9dfa035ffed3b87ce36d0c60d94c2c6ca6633dde
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-27 12:56:27 -0500

    Removed the HttpRequest overload

--- D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/Default/AuthTokenValidator.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/Default/AuthTokenValidator.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IdentityModel.Tokens;
 using System.Linq;
 using System.Security.Claims;
-using System.Web;
 using D2L.Security.AuthTokenValidation.JwtValidation;
 
 namespace D2L.Security.AuthTokenValidation.Default {
@@ -28,52 +26,10 @@ namespace D2L.Security.AuthTokenValidation.Default {
 
 			return ValidationResult.Success;
 		}
-
-		IGenericPrincipal IAuthTokenValidator.VerifyAndDecode( HttpRequest request ) {
-			try {
-				return VerifyAndDecodeWorker( request );
-			} catch( SecurityTokenExpiredException e ) {
-				throw new TokenExpiredException( "The provided token is expired", e );
-			} catch( Exception e ) {
-				throw new AuthorizationException( "An authorization exception has occured", e );
-			}
-		}
-
-		private IGenericPrincipal VerifyAndDecodeWorker( HttpRequest request ) {
-			string tokenFromCookie = GetTokenFromCookie( request );
-			string tokenFromAuthHeader = GetTokenFromAuthHeader( request );
-
-			if( tokenFromCookie != null && tokenFromAuthHeader != null ) {
-				throw new AuthorizationException( "Token cannot be provided in the header and cookie" );
-			}
-
-			return VerifyAndDecodeWorker( tokenFromCookie ?? tokenFromAuthHeader );
-		}
-
+		
 		private IGenericPrincipal VerifyAndDecodeWorker( string jwt ) {
 			IValidatedJwt validatedJwt = m_validator.Validate( jwt );
 			return GetPrincipal( validatedJwt, jwt );
-		}
-
-		internal static string GetTokenFromCookie( HttpRequest request ) {
-
-			HttpCookie cookie = request.Cookies.Get( "d2lApi" );
-			string authToken = null;
-			if( cookie != null ) {
-				authToken = cookie.Value;
-			}
-			return authToken;
-		}
-
-		internal static string GetTokenFromAuthHeader( HttpRequest request ) {
-
-			const string AUTH_HEADER_VALUE_PREFIX = "Bearer ";
-			string authorization = request.Headers.Get( "Authorization" );
-			string authToken = null;
-			if( authorization != null && authorization.StartsWith( AUTH_HEADER_VALUE_PREFIX ) ) {
-				authToken = authorization.Substring( AUTH_HEADER_VALUE_PREFIX.Length );
-			}
-			return authToken;
 		}
 
 		internal static Principal GetPrincipal( IValidatedJwt validatedJwt, string sourceJwt ) {
