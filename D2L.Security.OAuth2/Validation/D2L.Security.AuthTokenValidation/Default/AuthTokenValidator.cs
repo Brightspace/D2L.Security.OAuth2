1c51e763aab6203dade9bb2fa45339b1f679b65d
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-28 16:29:49 -0500

    Remove IGenericPrincipal from the stack

--- D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/Default/AuthTokenValidator.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/Default/AuthTokenValidator.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IdentityModel.Tokens;
-using System.Linq;
-using System.Security.Claims;
+﻿using System.IdentityModel.Tokens;
 using D2L.Security.AuthTokenValidation.JwtValidation;
 
 namespace D2L.Security.AuthTokenValidation.Default {
@@ -26,53 +23,6 @@ namespace D2L.Security.AuthTokenValidation.Default {
 			}
 
 			return ValidationResult.Success;
-		}
-		
-
-		/// <summary>
-		/// !!!!!!!!!! REMOVE!!!!!!!!!!!!
-		/// </summary>
-		/// <param name="validatedJwt"></param>
-		/// <param name="sourceJwt"></param>
-		/// <returns></returns>
-		internal static Principal GetPrincipal( IValidatedJwt validatedJwt, string sourceJwt ) {
-
-			string scopeClaimValue = validatedJwt.Claims
-				.Where( x => x.Type == "scope" )
-				.Select( x => x.Value )
-				.First();
-			HashSet<string> scopes = new HashSet<string>( 
-				scopeClaimValue.Split( ',' )
-				);
-
-			long userId = -1337;
-			Claim userIdClaim = validatedJwt.Claims.Where( x => x.Type == "uid" ).FirstOrDefault();
-			if( userIdClaim != null ) {
-				userId = long.Parse( userIdClaim.Value );
-			}
-
-			string tenantId = "00000000-0000-0000-0000-000000000000";
-			Claim tenantIdClaim = validatedJwt.Claims.Where( x => x.Type == "tid" ).FirstOrDefault();
-			if( tenantIdClaim != null ) {
-				tenantId = tenantIdClaim.Value;
-			}
-
-			string xsrfToken = "DUMMY XSRF TOKEN!!";
-			Claim xsrfClaim = validatedJwt.Claims.Where( x => x.Type == "xt" ).FirstOrDefault();
-			if( xsrfClaim != null ) {
-				xsrfToken = xsrfClaim.Value;
-			}
-
-			Principal principal = new Principal(
-				userId,
-				tenantId,
-				"localhost.com",
-				xsrfToken,
-				scopes,
-				sourceJwt
-				);
-
-			return principal;
 		}
 	}
 }
