fb65baf2d121a35fc2fb1946d5f847349376358b
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-01-29 13:11:02 -0500

    Extraction tests

--- D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication.Tests/Unit/Core/Default/IValidatedTokenExtensionsTests.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication.Tests/Unit/Core/Default/IValidatedTokenExtensionsTests.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Claims;
+using D2L.Security.AuthTokenValidation;
+using D2L.Security.RequestAuthentication.Core.Default;
+using Moq;
 using NUnit.Framework;
 
 namespace D2L.Security.RequestAuthentication.Tests.Unit.Core.Default {
@@ -10,49 +11,88 @@ namespace D2L.Security.RequestAuthentication.Tests.Unit.Core.Default {
 	[TestFixture]
 	internal sealed class IValidatedTokenExtensionsTests {
 
+		private void MockClaim( Mock<IValidatedToken> validatedTokenMock, string claimName, string claimValue ) {
+			Claim claim = new Claim( claimName, claimValue );
+			Claim[] claims = new Claim[] { claim };
+			validatedTokenMock.SetupGet( x => x.Claims ).Returns( claims );
+		}
+
 		[Test]
 		public void GetXsrfToken_Success() {
-			Assert.Inconclusive();
+			string expected = "somexsrf";
+			Mock<IValidatedToken> validatedTokenMock = new Mock<IValidatedToken>();
+			MockClaim( validatedTokenMock, Constants.Claims.XSRF, expected );
+			Assert.AreEqual( expected, IValidatedTokenExtensions.GetXsrfToken( validatedTokenMock.Object ) );
 		}
 
 		[Test]
 		public void GetXsrfToken_None_ReturnsNull() {
-			Assert.Inconclusive();
+			Mock<IValidatedToken> validatedTokenMock = new Mock<IValidatedToken>();
+			Assert.IsNull( IValidatedTokenExtensions.GetXsrfToken( validatedTokenMock.Object ) );
 		}
 
 		[Test]
 		public void GetTenantId_Success() {
-			Assert.Inconclusive();
+			string expected = "sometenantid";
+			Mock<IValidatedToken> validatedTokenMock = new Mock<IValidatedToken>();
+			MockClaim( validatedTokenMock, Constants.Claims.TENANT_ID, expected );
+			Assert.AreEqual( expected, IValidatedTokenExtensions.GetTenantId( validatedTokenMock.Object ) );
 		}
 
 		[Test]
 		public void GetTenantId_None_ReturnsNull() {
-			Assert.Inconclusive();
+			Mock<IValidatedToken> validatedTokenMock = new Mock<IValidatedToken>();
+			Assert.IsNull( IValidatedTokenExtensions.GetTenantId( validatedTokenMock.Object ) );
 		}
 
 		[Test]
 		public void GetScopes_One_Success() {
-			Assert.Inconclusive();
+			string expected = "somescope";
+			Mock<IValidatedToken> validatedTokenMock = new Mock<IValidatedToken>();
+			MockClaim( validatedTokenMock, Constants.Claims.SCOPE, expected );
+			IEnumerable<string> scopes = IValidatedTokenExtensions.GetScopes( validatedTokenMock.Object );
+			Assert.AreEqual( 1, scopes.Count() );
+			Assert.AreEqual( expected, scopes.First() );
 		}
 
 		[Test]
 		public void GetScopes_Many_Success() {
-			Assert.Inconclusive();
+			string scope1 = "somescope1";
+			string scope2 = "somescope2";
+			Mock<IValidatedToken> validatedTokenMock = new Mock<IValidatedToken>();
+			MockClaim( validatedTokenMock, Constants.Claims.SCOPE, scope1 + " " + scope2 );
+			IEnumerable<string> scopes = IValidatedTokenExtensions.GetScopes( validatedTokenMock.Object );
+			Assert.AreEqual( 2, scopes.Count() );
+			Assert.IsTrue( scopes.Contains( scope1 ) );
+			Assert.IsTrue( scopes.Contains( scope2 ) );
 		}
 
 		[Test]
 		public void GetScopes_None_ReturnsEmpty() {
-			Assert.Inconclusive();
+			Mock<IValidatedToken> validatedTokenMock = new Mock<IValidatedToken>();
+			IEnumerable<string> scopes = IValidatedTokenExtensions.GetScopes( validatedTokenMock.Object );
+			Assert.IsFalse( scopes.Any() );
 		}
 
 		[Test]
 		public void GetUserId_Success() {
-			Assert.Inconclusive();
+			long expected = 1337;
+			Mock<IValidatedToken> validatedTokenMock = new Mock<IValidatedToken>();
+			MockClaim( validatedTokenMock, Constants.Claims.USER_ID, expected.ToString() );
+			Assert.AreEqual( expected, IValidatedTokenExtensions.GetUserId( validatedTokenMock.Object ) );
+		}
+
+		[Test]
+		public void GetUserId_NonNumeric_ReturnsNull() {
+			Mock<IValidatedToken> validatedTokenMock = new Mock<IValidatedToken>();
+			MockClaim( validatedTokenMock, Constants.Claims.USER_ID, "nonnumeric_userid" );
+			Assert.IsNull( IValidatedTokenExtensions.GetUserId( validatedTokenMock.Object ) );
 		}
 
 		[Test]
 		public void GetUserId_None_ReturnsNull() {
-			Assert.Inconclusive();
+			Mock<IValidatedToken> validatedTokenMock = new Mock<IValidatedToken>();
+			Assert.IsNull( IValidatedTokenExtensions.GetUserId( validatedTokenMock.Object ) );
 		}
 	}
 }
