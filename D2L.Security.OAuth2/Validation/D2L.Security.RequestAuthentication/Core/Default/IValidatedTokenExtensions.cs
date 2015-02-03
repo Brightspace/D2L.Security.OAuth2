96f532cd12a675afe4d0f1a143ae201c65bb44a9
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-03 14:22:07 -0500

    Code review changes

--- D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication/Core/Default/IValidatedTokenExtensions.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication/Core/Default/IValidatedTokenExtensions.cs
@@ -6,36 +6,24 @@ using D2L.Security.AuthTokenValidation;
 namespace D2L.Security.RequestAuthentication.Core.Default {
 	internal static class IValidatedTokenExtensions {
 
-		/// <summary>
-		/// Returns the Xsrf token from a validated token.
-		/// </summary>
 		/// <param name="token">A validated token</param>
 		/// <returns>The value of the Xsrf token. Returns null if one was not found.</returns>
 		internal static string GetXsrfToken( this IValidatedToken token ) {
 			return token.GetClaimValue( Constants.Claims.XSRF );
 		}
 
-		/// <summary>
-		/// Returns the tenant id from a validated token.
-		/// </summary>
 		/// <param name="token">A validated token</param>
 		/// <returns>The tenant id. Returns null if one was not found.</returns>
 		internal static string GetTenantId( this IValidatedToken token ) {
 			return token.GetClaimValue( Constants.Claims.TENANT_ID );
 		}
 
-		/// <summary>
-		/// Returns the tenant url from a validated token.
-		/// </summary>
 		/// <param name="token">A validated token</param>
 		/// <returns>The tenant url. Returns null if one was not found.</returns>
 		internal static string GetTenantUrl( this IValidatedToken token ) {
 			return token.GetClaimValue( Constants.Claims.TENANT_URL );
 		}
 
-		/// <summary>
-		/// Returns the scopes from a validated token.
-		/// </summary>
 		/// <param name="token">A validated token</param>
 		/// <returns>The scopes</returns>
 		internal static IEnumerable<string> GetScopes( this IValidatedToken token ) {
@@ -49,9 +37,6 @@ namespace D2L.Security.RequestAuthentication.Core.Default {
 			return scopesArray;
 		}
 
-		/// <summary>
-		/// Returns the user id from a validated token.
-		/// </summary>
 		/// <param name="token">A validated token</param>
 		/// <returns>The user id. Returns null if one was not found.</returns>
 		internal static long? GetUserId( this IValidatedToken token ) {
@@ -69,9 +54,6 @@ namespace D2L.Security.RequestAuthentication.Core.Default {
 			return result;
 		}
 
-		/// <summary>
-		/// Gets the value of the claim with the specified name
-		/// </summary>
 		/// <param name="token">A validated token</param>
 		/// <param name="claimName">The name of the claim whose value is returned</param>
 		/// <returns>The claim value</returns>
