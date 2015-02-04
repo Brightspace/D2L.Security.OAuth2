98ee3a4daff3ac5f0546c5e628c9ffaab171931d
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-04 16:05:18 -0500

    Nuget

--- D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/2publishNugetPackage.bat
+++ D2L.Security.OAuth2/Validation/D2L.Security.AuthTokenValidation/2publishNugetPackage.bat
@@ -1,3 +1,3 @@
-..\.nuget\nuget push D2L.Security.AuthTokenValidation.1.1.0.0.nupkg REDACTED-nuget-publish-key -s http://nuget.build.d2l/nuget/stable
+..\.nuget\nuget push D2L.Security.AuthTokenValidation.1.1.1.0.nupkg REDACTED-nuget-publish-key -s http://nuget.build.d2l/nuget/stable
 
 pause
