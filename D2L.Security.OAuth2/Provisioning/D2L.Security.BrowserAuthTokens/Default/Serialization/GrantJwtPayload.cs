8ae4a639981a54127c999cd396b35e4bca9ab6b8
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-05 16:58:25 -0500

    Unstable. Enabled public documentation

---
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.BrowserAuthTokens/Default/Serialization/GrantJwtPayload.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace D2L.Security.BrowserAuthTokens.Default.Serialization {
+	[DataContract]
+	internal sealed class GrantJwtPayload {
+
+		private readonly long m_expiry;
+		private readonly string m_userId;
+		private readonly string m_tenantId;
+		private readonly string m_tenantUrl;
+
+		internal GrantJwtPayload(
+			string userId,
+			string tenantId,
+			string tenantUrl
+			) {
+
+			m_userId = userId;
+			m_tenantId = tenantId;
+			m_tenantUrl = tenantUrl;
+
+			DateTime expiry = DateTime.UtcNow + TimeSpan.FromMinutes( 30 );
+			m_expiry = expiry.GetSecondsSinceUnixEpoch();
+		}
+
+		[DataMember]
+		public string iss {
+			get { return "lms.dev.d2l"; }
+			private set { }
+		}
+
+		[DataMember]
+		public string sub {
+			get { return m_userId; }
+			set { }
+		}
+
+		[DataMember]
+		public long exp {
+			get { return m_expiry; }
+			set { }
+		}
+		
+		[DataMember]
+		public string aud {
+			get { return "https://api.brightspace.com/auth/token"; }
+			private set { }
+		}
+
+		[DataMember]
+		public string tenantid {
+			get { return m_tenantId; }
+			set { }
+		}
+
+		[DataMember]
+		public string tenanturl {
+			get { return m_tenantUrl; }
+			set { }
+		}
+	}
+}
---
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.BrowserAuthTokens/Default/Serialization/SerializationHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace D2L.Security.BrowserAuthTokens.Default.Serialization {
+	internal static class SerializationHelper {
+		internal static string Serialize<T>( T source ) {
+			DataContractJsonSerializer serializer = new DataContractJsonSerializer( typeof( T ) );
+
+			using( MemoryStream stream = new MemoryStream() ) {
+				serializer.WriteObject( stream, source );
+				string result = Encoding.UTF8.GetString( stream.ToArray() );
+
+				return result;
+			}
+		}
+	}
+}
