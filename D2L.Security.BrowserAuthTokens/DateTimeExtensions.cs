97951fd406b94ffc17d633294dccb9752d6a17c1
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-05 16:58:25 -0500

    Unstable. Enabled public documentation

--- D2L.Security.BrowserAuthTokens/DateTimeExtensions.cs
+++ D2L.Security.BrowserAuthTokens/DateTimeExtensions.cs
@@ -7,9 +7,7 @@ namespace D2L.Security.BrowserAuthTokens {
 			new DateTime( 1970, 1, 1, 0, 0, 0, DateTimeKind.Utc );
 
 		/// <param name="date">The date</param>
-		/// <returns>
-		/// Number of seconds since the beginning of the Unix Epoch to the specified date
-		/// </returns>
+		/// <returns>Number of seconds since the beginning of the Unix Epoch to the specified date</returns>
 		internal static long GetSecondsSinceUnixEpoch( this DateTime date ) {
 			TimeSpan sinceUnixEpoch = date - UNIX_EPOCH_START;
 			return (long)sinceUnixEpoch.TotalSeconds;
