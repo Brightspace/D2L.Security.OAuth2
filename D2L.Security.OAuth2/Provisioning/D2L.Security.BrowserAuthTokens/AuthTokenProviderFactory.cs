ed455fba76a80c44b727fb807f026c3f41e3fa6c
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-05 13:43:48 -0500

    Rough initial work

---
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.BrowserAuthTokens/AuthTokenProviderFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using D2L.Security.BrowserAuthTokens.Default;
+
+namespace D2L.Security.BrowserAuthTokens {
+	public static class AuthTokenProviderFactory {
+		
+		public static IAuthTokenProvider Create( 
+			IAssertionGrantSigner signer, 
+			Uri authServiceEndpoint 
+			) {
+
+			return new AuthTokenProvider();
+		}
+	}
+}
