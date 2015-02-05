a6ab8daecb9f0321a466fe4395fce3b3f0c866fe
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-05 14:03:53 -0500

    Added test project and Nunit dependency

---
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.BrowserAuthTokens.Tests/TemporaryTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D2L.Security.BrowserAuthTokens.Default;
+using NUnit.Framework;
+
+namespace D2L.Security.BrowserAuthTokens.Tests {
+	
+	[TestFixture]
+	internal sealed class TemporaryTest {
+
+		[Test]
+		public void THROWAWAY_TEST() {
+			AuthServerInvoker.TEST();
+		}
+	}
+}
