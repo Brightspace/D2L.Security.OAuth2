cee2968a1045609429c8e89113280190be8b210a
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-05 14:03:53 -0500

    Added test project and Nunit dependency

---
+++ D2L.Security.BrowserAuthTokens.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D2L.Security.BrowserAuthTokens.Tests {
+	class Program {
+		static void Main( string[] args ) {
+		}
+	}
+}
