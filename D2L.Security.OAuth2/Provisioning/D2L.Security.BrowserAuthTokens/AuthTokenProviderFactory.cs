45ec273175d413a23469c2dc32a560a6e7e51aa5
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-09 17:59:42 -0500

    Refactoring... still unstable.

--- D2L.Security.OAuth2/Provisioning/D2L.Security.BrowserAuthTokens/AuthTokenProviderFactory.cs
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.BrowserAuthTokens/AuthTokenProviderFactory.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Security.Cryptography.X509Certificates;
 using D2L.Security.BrowserAuthTokens.Default;
+using D2L.Security.BrowserAuthTokens.Invocation;
 
 namespace D2L.Security.BrowserAuthTokens {
 	public static class AuthTokenProviderFactory {
 		
 		public static IAuthTokenProvider Create(
 			X509Certificate2 certificate,
-			Uri authServiceEndpoint
+			Uri tokenProvisioningEndpoint
 			) {
 
-			return new AuthTokenProvider( certificate );
+			IAuthServiceInvoker serviceInvoker = AuthServiceInvokerFactory.Create( tokenProvisioningEndpoint );
+			return new AuthTokenProvider( certificate, serviceInvoker );
 		}
 	}
 }
---
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.BrowserAuthTokens/ProvisioningParameters.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace D2L.Security.BrowserAuthTokens {
+
+	/// <summary>
+	/// Configures assertion grant token
+	/// </summary>
+	public sealed class ProvisioningParameters {
+
+		private readonly string m_clientId;
+		private readonly string m_clientSecret;
+		private readonly IEnumerable<string> m_scopes;
+
+		private readonly string m_tenantId;
+		private readonly string m_tenantUrl;
+		private readonly DateTime m_expiry;
+
+		public ProvisioningParameters( 
+			string clientId, 
+			string clientSecret, 
+			IEnumerable<string> scopes, 
+			string tenantId,
+			string tenantUrl
+			) {
+
+			m_clientId = clientId;
+			m_clientSecret = clientSecret;
+			m_scopes = scopes;
+			m_tenantId = tenantId;
+			m_tenantUrl = tenantUrl;
+
+			m_expiry = DateTime.UtcNow + Constants.AssertionGrant.ASSERTION_TOKEN_LIFETIME;
+		}
+
+		public string UserId { get; set; }
+		public string Xsrf { get; set; }
+
+		public string ClientId {
+			get { return m_clientId; }
+		}
+
+		public string ClientSecret {
+			get { return m_clientSecret; }
+		}
+
+		public IEnumerable<string> Scopes {
+			get { return m_scopes; }
+		}
+
+		public string TenantId {
+			get { return m_tenantId; }
+		}
+
+		public string TenantUrl {
+			get { return m_tenantUrl; }
+		}
+
+		public DateTime Expiry {
+			get { return m_expiry; }
+		}
+	}
+}
