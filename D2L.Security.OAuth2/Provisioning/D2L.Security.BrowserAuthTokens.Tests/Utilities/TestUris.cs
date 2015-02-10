2be9a397da237608e3e570f09ae34a8b7a93c6fd
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-10 13:52:01 -0500

    Removed dead code. Cleaned up test. Fixed tests project output type.

--- D2L.Security.OAuth2/Provisioning/D2L.Security.BrowserAuthTokens.Tests/Utilities/TestUris.cs
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.BrowserAuthTokens.Tests/Utilities/TestUris.cs
@@ -6,6 +6,5 @@ namespace D2L.Security.BrowserAuthTokens.Tests.Utilities {
 		private static readonly Uri AUTH_SERVER_SITE = new Uri( "https://auth.proddev.d2l:44331" );
 
 		internal static readonly Uri AUTH_TOKEN_PROVISIONING_URI = new Uri( AUTH_SERVER_SITE, "core/connect/token/" );
-		internal static readonly Uri TOKEN_VERIFICATION_AUTHORITY_URI = new Uri( AUTH_SERVER_SITE, "core/" );
 	}
 }
