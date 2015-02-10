29bd2717975fefdc5d7097ff63f27175f523d06d
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-10 12:58:58 -0500

    Deserialize access token

---
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.BrowserAuthTokens/IAccessToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace D2L.Security.BrowserAuthTokens {
+	
+	public interface IAccessToken {
+		string Token { get; }
+
+		/// <summary>
+		/// Lifetime of this access token
+		/// </summary>
+		TimeSpan ExpiresIn { get; }
+	}
+}
