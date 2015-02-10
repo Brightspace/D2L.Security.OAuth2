a5827f8d4f462e3506e434ca9402ebdd736a64a5
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-10 13:52:01 -0500

    Removed dead code. Cleaned up test. Fixed tests project output type.

---
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.BrowserAuthTokens.Tests/Integration/FullStack/FullStackTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using D2L.Security.BrowserAuthTokens.Default;
+using D2L.Security.BrowserAuthTokens.Invocation;
+using D2L.Security.BrowserAuthTokens.Tests.Utilities;
+using NUnit.Framework;
+
+namespace D2L.Security.BrowserAuthTokens.Tests.Integration.FullStack {
+	
+	[TestFixture]
+	internal sealed class FullStackTests {
+
+		private readonly IAuthTokenProvider m_tokenProvider = 
+			AuthTokenProviderFactory.Create(
+				TestCredentials.LMS.CERTIFICATE,
+				TestUris.AUTH_TOKEN_PROVISIONING_URI
+				);
+
+		[Test]
+		public void IAuthTokenProvider_ProvisionAccessTokenAsync_Success() {
+			string userId = "smUser";
+			string tenantId = "smTenant";
+			string tenantUrl = "smTenantUrl";
+			string xsrf = "smXsrf";
+
+			ProvisioningParameters provisioningParams = new ProvisioningParameters(
+				TestCredentials.LMS.CLIENT_ID,
+				TestCredentials.LMS.CLIENT_SECRET,
+				new string[] { TestCredentials.LOReSScopes.MANAGE },
+				tenantId,
+				tenantUrl
+				);
+
+			provisioningParams.UserId = userId;
+			provisioningParams.Xsrf = xsrf;
+
+			Task<IAccessToken> task = m_tokenProvider.ProvisionAccessTokenAsync( provisioningParams );
+			IAccessToken accessToken = task.Result;
+
+
+		}
+	}
+}
--- D2L.Security.OAuth2/Provisioning/D2L.Security.BrowserAuthTokens.Tests/Utilities/TestCredentials.cs
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.BrowserAuthTokens.Tests/Utilities/TestCredentials.cs
@@ -1,4 +1,6 @@
-﻿namespace D2L.Security.BrowserAuthTokens.Tests.Utilities {
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace D2L.Security.BrowserAuthTokens.Tests.Utilities {
 	internal static class TestCredentials {
 
 		internal static class LOReSScopes {
@@ -6,6 +8,12 @@
 		}
 
 		internal static class LMS {
+			internal static readonly X509Certificate2 CERTIFICATE =
+				new X509Certificate2(
+					Resources.lms_dev_d2l,
+					Resources.lms_dev_d2l_PASSWORD
+					);
+
 			internal const string CLIENT_ID = "lms.dev.d2l";
 			internal const string CLIENT_SECRET = "lms_secret";
 		}
