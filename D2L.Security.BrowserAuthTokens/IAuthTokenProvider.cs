2059ce7137d15cddc25858e679ed7da0b6dae45c
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-10 12:58:58 -0500

    Deserialize access token

--- D2L.Security.BrowserAuthTokens/IAuthTokenProvider.cs
+++ D2L.Security.BrowserAuthTokens/IAuthTokenProvider.cs
@@ -3,12 +3,7 @@
 namespace D2L.Security.BrowserAuthTokens {
 	
 	public interface IAuthTokenProvider {
-		Task<string> ProvisionAccessToken( ProvisioningParameters provisioningParams );
 
-		// no xsrf
-		//Task<string> GetTokenForUserAsync( string tenantId, long userId );
-
-		// no user id, no xsrf (application to service)
-		//Task<string> GetTokenForApplicationAsync( string tenantId );
+		Task<IAccessToken> ProvisionAccessTokenAsync( ProvisioningParameters provisioningParams );
 	}
 }
