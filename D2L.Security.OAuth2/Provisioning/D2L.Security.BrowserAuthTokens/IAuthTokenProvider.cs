620427cdcf94836e617ba576fa3c8e5bb03af187
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-11 14:04:35 -0500

    Introduced a parallel, synchronous stack. Synchronous clients won't have to incur the async/await overhead or risk deadlocks from synchronously waiting on the async calls.

--- D2L.Security.OAuth2/Provisioning/D2L.Security.BrowserAuthTokens/IAuthTokenProvider.cs
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.BrowserAuthTokens/IAuthTokenProvider.cs
@@ -5,5 +5,6 @@ namespace D2L.Security.BrowserAuthTokens {
 	public interface IAuthTokenProvider {
 
 		Task<IAccessToken> ProvisionAccessTokenAsync( ProvisioningParameters provisioningParams );
+		IAccessToken ProvisionAccessToken( ProvisioningParameters provisioningParams );
 	}
 }
