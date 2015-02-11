91ac027cdd41298583ebe857cefadb491f57cc5b
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-11 17:39:44 -0500

    Plural...

--- D2L.Security.OAuth2/Provisioning/D2L.Security.AuthTokenProvisioning/Invocation/IAuthServiceInvoker.cs
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.AuthTokenProvisioning/Invocation/IAuthServiceInvoker.cs
@@ -3,7 +3,7 @@
 namespace D2L.Security.AuthTokenProvisioning.Invocation {
 	
 	/// <summary>
-	/// Makes invocations on the Auth Service to provision access token
+	/// Makes invocations on the Auth Service to provision access tokens
 	/// </summary>
 	interface IAuthServiceInvoker {
 
