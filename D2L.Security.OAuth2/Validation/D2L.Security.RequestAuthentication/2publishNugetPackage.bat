11036bed1303b0caab5ec583ddc7a2788b0d737d
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-17 15:09:11 -0500

    Nuget 1.1.2.0

--- D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication/2publishNugetPackage.bat
+++ D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication/2publishNugetPackage.bat
@@ -1,4 +1,4 @@
 
-..\.nuget\nuget push D2L.Security.RequestAuthentication.1.1.1.0.nupkg REDACTED-nuget-publish-key -s http://nuget.build.d2l/nuget/stable
+..\.nuget\nuget push D2L.Security.RequestAuthentication.1.1.2.0.nupkg REDACTED-nuget-publish-key -s http://nuget.build.d2l/nuget/stable
 
 pause
--- D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication/Properties/AssemblyInfo.cs
+++ D2L.Security.OAuth2/Validation/D2L.Security.RequestAuthentication/Properties/AssemblyInfo.cs
@@ -13,8 +13,8 @@
 [assembly: AssemblyProduct( "Brightspace" )]
 
 // Nuget: Version
-[assembly: AssemblyInformationalVersion( "1.1.1.0" )]
-[assembly: AssemblyVersion( "1.1.1.0" )]
-[assembly: AssemblyFileVersion( "1.1.1.0" )]
+[assembly: AssemblyInformationalVersion( "1.1.2.0" )]
+[assembly: AssemblyVersion( "1.1.2.0" )]
+[assembly: AssemblyFileVersion( "1.1.2.0" )]
 
 [assembly: AssemblyCopyright( "Copyright © Desire2Learn" )]
