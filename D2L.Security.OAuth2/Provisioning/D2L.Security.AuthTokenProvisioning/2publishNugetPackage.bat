4944d1441c4f9ca9521e8ee65e630960a3faa674
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-17 14:49:20 -0500

    Nuget

--- D2L.Security.OAuth2/Provisioning/D2L.Security.AuthTokenProvisioning/2publishNugetPackage.bat
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.AuthTokenProvisioning/2publishNugetPackage.bat
@@ -1,3 +1,3 @@
-..\.nuget\nuget push D2L.Security.AuthTokenProvisioning.1.0.2.0.nupkg REDACTED-nuget-publish-key -s http://nuget.build.d2l/nuget/stable
+..\.nuget\nuget push D2L.Security.AuthTokenProvisioning.1.0.2.13.nupkg REDACTED-nuget-publish-key -s http://nuget.build.d2l/nuget/stable
 
 pause
