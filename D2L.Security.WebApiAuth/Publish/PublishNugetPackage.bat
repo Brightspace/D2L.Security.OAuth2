7edf93ba85ad6dcf87298235c9b88fe9c801358b
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-18 10:37:53 -0500

    1.0.7.0

--- D2L.Security.WebApiAuth/Publish/PublishNugetPackage.bat
+++ D2L.Security.WebApiAuth/Publish/PublishNugetPackage.bat
@@ -1,4 +1,4 @@
 
-..\..\.nuget\nuget push D2L.Security.WebApiAuth.1.0.6.0.nupkg REDACTED-nuget-publish-key -s http://nuget.build.d2l/nuget/stable
+..\..\.nuget\nuget push D2L.Security.WebApiAuth.1.0.7.0.nupkg REDACTED-nuget-publish-key -s http://nuget.build.d2l/nuget/stable
 
 pause
