a24dd31e89601ded888a99ae3a90050753f97853
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-02-20 10:56:04 -0500

    Added reference to moq

---
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.AuthTokenProvisioning.Tests/Unit/Default/AuthTokenProviderTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D2L.Security.AuthTokenProvisioning.Invocation;
+using NUnit.Framework;
+
+namespace D2L.Security.AuthTokenProvisioning.Tests.Unit.Default {
+	
+	[TestFixture]
+	internal sealed class AuthTokenProviderTests {
+
+		[Test]
+		public void a() {
+			//Mock<IAuthServiceInvoker>
+		}
+	}
+}
