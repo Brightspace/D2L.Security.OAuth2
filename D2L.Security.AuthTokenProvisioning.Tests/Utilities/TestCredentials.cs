d8eeb995bffe9e15d1ee3287a6d817af80a69700
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-03-04 16:38:51 -0500

    Tests no longer need the hardcoded .pfx certificate

--- D2L.Security.AuthTokenProvisioning.Tests/Utilities/TestCredentials.cs
+++ D2L.Security.AuthTokenProvisioning.Tests/Utilities/TestCredentials.cs
@@ -1,6 +1,4 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace D2L.Security.AuthTokenProvisioning.Tests.Utilities {
+﻿namespace D2L.Security.AuthTokenProvisioning.Tests.Utilities {
 	internal static class TestCredentials {
 
 		internal static class LOReSScopes {
@@ -8,12 +6,6 @@ namespace D2L.Security.AuthTokenProvisioning.Tests.Utilities {
 		}
 
 		internal static class LMS {
-			internal static readonly X509Certificate2 CERTIFICATE =
-				new X509Certificate2(
-					Resources.lms_dev_d2l,
-					Resources.lms_dev_d2l_PASSWORD
-					);
-
 			internal const string CLIENT_ID = "lms.dev.d2l";
 			internal const string CLIENT_SECRET = "lms_secret";
 		}
