574de1dd517b23af74eeafdda1edc0843d6350c0
Khaled Hammouda <khaled.hammouda@d2l.com>
2015-03-09 18:03:49 -0400

    US43092 - Scope authorization

---
+++ D2L.Security.OAuth2/Scopes/src/D2L.Security.ScopeAuthorizatoin/Scope.cs
@@ -0,0 +1,33 @@
+﻿namespace D2L.Security.ScopeAuthorization {
+
+	internal sealed class Scope {
+
+		private readonly string m_group;
+		private readonly string m_resource;
+		private readonly string[] m_permissions;
+
+		public Scope( string group, string resource, string permission )
+			: this( group, resource, new[] { permission } ) {
+		}
+
+		public Scope( string group, string resource, string[] permissions ) {
+			m_group = group;
+			m_resource = resource;
+			m_permissions = permissions;
+		}
+
+		public string Group {
+			get { return m_group; }
+		}
+
+		public string Resource {
+			get { return m_resource; }
+		}
+
+		public string[] Permissions {
+			get { return m_permissions; }
+		}
+
+	}
+
+}
