5b7603acae29b4f081f6d06e186b95a82a60e507
Khaled Hammouda <khaled.hammouda@d2l.com>
2015-03-10 15:17:03 -0400

    US43092 - Refactored ScopeAuthorizationAttributeTests to be less repetitive

--- D2L.Security.OAuth2/Scopes/test/D2L.Security.ScopeAuthorization.Tests/ScopeAuthorizeAttributeTests.cs
+++ D2L.Security.OAuth2/Scopes/test/D2L.Security.ScopeAuthorization.Tests/ScopeAuthorizeAttributeTests.cs
@@ -13,17 +13,64 @@ namespace D2L.Security.ScopeAuthorization.Tests {
 
 	internal sealed class ScopeAuthorizeAttributeTests {
 
-		private static readonly Collection<AllowAnonymousAttribute> AllowAnonymousAttributeCollection =
-			new Collection<AllowAnonymousAttribute>( Enumerable.Empty<AllowAnonymousAttribute>().ToList() );
+		[Test]
+		public void PrincipalNotSet_AuthorizationShouldBeDenied() {
 
-		private readonly HttpActionContext m_actionContext;
+			var actionContext = CreateActionContextWithPrinciapl( null );
 
-		public ScopeAuthorizeAttributeTests() {
+			var attr = new ScopeAuthorizeAttribute( "g", "r", "p" );
+			attr.OnAuthorization( actionContext );
+
+			AssertIsNotAuthorized( actionContext.Response );
+		}
+
+		[Test]
+		public void NoScopesGranted_AuthorizationShouldBeDenied() {
+
+			var principal = CreatePrinciaplWithScopeClaim( null );
+			var actionContext = CreateActionContextWithPrinciapl( principal );
+
+			var attr = new ScopeAuthorizeAttribute( "g", "r", "p" );
+			attr.OnAuthorization( actionContext );
+
+			AssertIsNotAuthorized( actionContext.Response );
+		}
+
+		[Test]
+		public void RequiredScopeIsNotGranted_AuthorizationShouldBeDenied() {
+
+			var principal = CreatePrinciaplWithScopeClaim( "g:r:x" );
+			var actionContext = CreateActionContextWithPrinciapl( principal );
+
+			var attr = new ScopeAuthorizeAttribute( "g", "r", "p" );
+			attr.OnAuthorization( actionContext );
+
+			AssertIsNotAuthorized( actionContext.Response );
+		}
+
+		[Test]
+		public void RequiredScopeIsGranted_AuthorizationShouldBeGranted() {
+
+			var principal = CreatePrinciaplWithScopeClaim( "g:r:p" );
+			var actionContext = CreateActionContextWithPrinciapl( principal );
+
+			var attr = new ScopeAuthorizeAttribute( "g", "r", "p" );
+			attr.OnAuthorization( actionContext );
+
+			AssertIsAuthorized( actionContext.Response );
+		}
+
+		#region Helpers
+
+		private static HttpActionContext CreateActionContextWithPrinciapl( ClaimsPrincipal princiapl ) {
+
+			var allowAnonymousAttributeCollection =
+				new Collection<AllowAnonymousAttribute>( Enumerable.Empty<AllowAnonymousAttribute>().ToList() );
 
 			var controllerDescriptorMock = new Mock<HttpControllerDescriptor> { CallBase = true };
 			controllerDescriptorMock
 				.Setup( cd => cd.GetCustomAttributes<AllowAnonymousAttribute>() )
-				.Returns( AllowAnonymousAttributeCollection );
+				.Returns( allowAnonymousAttributeCollection );
 
 			var controllerContext = new Mock<HttpControllerContext> { CallBase = true }.Object;
 			controllerContext.ControllerDescriptor = controllerDescriptorMock.Object;
@@ -32,76 +79,38 @@ namespace D2L.Security.ScopeAuthorization.Tests {
 			var actionDescriptorMock = new Mock<HttpActionDescriptor> { CallBase = true };
 			actionDescriptorMock
 				.Setup( ad => ad.GetCustomAttributes<AllowAnonymousAttribute>() )
-				.Returns( AllowAnonymousAttributeCollection );
+				.Returns( allowAnonymousAttributeCollection );
 
-			m_actionContext = ContextUtil.CreateActionContext( controllerContext, actionDescriptorMock.Object );
+			var actionContext = ContextUtil.CreateActionContext( controllerContext, actionDescriptorMock.Object );
+
+			actionContext.ControllerContext.RequestContext.Principal = princiapl;
+
+			return actionContext;
 		}
 
-		[Test]
-		public void PrincipalNotSet_AuthorizationShouldBeDenied() {
+		private static ClaimsPrincipal CreatePrinciaplWithScopeClaim( string scopePattern ) {
 
-			m_actionContext.ControllerContext.RequestContext.Principal = null;
-
-			var attr = new ScopeAuthorizeAttribute( "g", "r", "p" );
-			attr.OnAuthorization( m_actionContext );
-
-			// response should be 403
-			m_actionContext.Response.Should().NotBeNull();
-			m_actionContext.Response.StatusCode.Should().Be( HttpStatusCode.Forbidden );
-		}
-
-		[Test]
-		public void NoScopesGranted_AuthorizationShouldBeDenied() {
-
-			// create claims principal
-			var claims = Enumerable.Empty<Claim>();
+			var claims = ( scopePattern == null )
+				? Enumerable.Empty<Claim>()
+				: new[] { new Claim( Constants.ClaimTypes.Scope, scopePattern ) };
 			var identity = new ClaimsIdentity( claims );
 			var principal = new ClaimsPrincipal( identity );
-			// assign principal to request context
-			m_actionContext.ControllerContext.RequestContext.Principal = principal;
 
-			var attr = new ScopeAuthorizeAttribute( "g", "r", "p" );
-			attr.OnAuthorization( m_actionContext );
-
-			// response should be 403
-			m_actionContext.Response.Should().NotBeNull();
-			m_actionContext.Response.StatusCode.Should().Be( HttpStatusCode.Forbidden );
+			return principal;
 		}
 
-		[Test]
-		public void RequiredScopeIsNotGranted_AuthorizationShouldBeDenied() {
-
-			// create claims principal
-			var claims = new[] { new Claim( Constants.ClaimTypes.Scope, "g:r:x" ) };
-			var identity = new ClaimsIdentity( claims );
-			var principal = new ClaimsPrincipal( identity );
-			// assign principal to request context
-			m_actionContext.ControllerContext.RequestContext.Principal = principal;
-
-			var attr = new ScopeAuthorizeAttribute( "g", "r", "p" );
-			attr.OnAuthorization( m_actionContext );
-
-			// response should be 403
-			m_actionContext.Response.Should().NotBeNull();
-			m_actionContext.Response.StatusCode.Should().Be( HttpStatusCode.Forbidden );
-		}
-
-		[Test]
-		public void RequiredScopeIsGranted_AuthorizationShouldBeGranted() {
-
-			// create claims principal
-			var claims = new[] { new Claim( Constants.ClaimTypes.Scope, "g:r:p" ) };
-			var identity = new ClaimsIdentity( claims );
-			var principal = new ClaimsPrincipal( identity );
-			// assign principal to request context
-			m_actionContext.ControllerContext.RequestContext.Principal = principal;
-
-			var attr = new ScopeAuthorizeAttribute( "g", "r", "p" );
-			attr.OnAuthorization( m_actionContext );
-
+		private static void AssertIsAuthorized( HttpResponseMessage response ) {
 			// response should be null (i.e. not short-circuited with a 403 by attribute)
-			m_actionContext.Response.Should().BeNull();
+			response.Should().BeNull();
 		}
+
+		private static void AssertIsNotAuthorized( HttpResponseMessage response ) {
+			// response should be 403
+			response.Should().NotBeNull();
+			response.StatusCode.Should().Be( HttpStatusCode.Forbidden );
+		}
+
+		#endregion
 
 	}
 
