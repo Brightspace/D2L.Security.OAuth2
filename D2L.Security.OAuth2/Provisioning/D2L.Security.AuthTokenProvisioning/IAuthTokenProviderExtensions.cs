5032628a56e97a7f63c62bbc9411485009dd3742
Owen Smith <owen.smith@d2l.com>
2015-03-19 17:19:15 -0400

    refact(tokens) create static class SigningTokenFactory
    * move CreateSigningToken helper here
    * add an overload for RSA to the helper

--- D2L.Security.OAuth2/Provisioning/D2L.Security.AuthTokenProvisioning/IAuthTokenProviderExtensions.cs
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.AuthTokenProvisioning/IAuthTokenProviderExtensions.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IdentityModel.Tokens;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace D2L.Security.AuthTokenProvisioning {
 	public static class IAuthTokenProviderExtensions {
@@ -15,20 +11,6 @@ namespace D2L.Security.AuthTokenProvisioning {
 			SecurityToken signingToken
 		) {
 			var token = @this.ProvisionAccessTokenAsync( claimSet, scopes, signingToken ).Result;
-			return token;
-		}
-
-		// I know this isn't actually an extension, but mrr
-		public static SecurityToken CreateSigningToken(
-			this IAuthTokenProvider _,
-			SecurityKey key,
-			Guid keyId
-		) {
-			var token = new KidSecurityToken(
-				keyId.ToString(),
-				key
-			);
-
 			return token;
 		}
 
