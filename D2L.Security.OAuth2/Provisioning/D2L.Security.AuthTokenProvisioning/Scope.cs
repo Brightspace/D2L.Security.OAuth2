4e79f1fee797eaccada706954f06685be1b835c9
Owen Smith <owen.smith@d2l.com>
2015-03-20 11:19:55 -0400

    refact(provider) add extensions for (temporaily) supporting IE<string> scopes

--- D2L.Security.OAuth2/Provisioning/D2L.Security.AuthTokenProvisioning/Scope.cs
+++ D2L.Security.OAuth2/Provisioning/D2L.Security.AuthTokenProvisioning/Scope.cs
@@ -21,13 +21,45 @@ namespace D2L.Security.AuthTokenProvisioning {
 		public Scope( string group, string resource, string[] permissions ) {
 			m_group = group;
 			m_resource = resource;
-			m_permissions = permissions;
+			m_permissions = permissions.OrderBy( x => x ).ToArray();
 		}
 
 		public override string ToString() {
 			var permissionsString = string.Join( ",", m_permissions );
 			var result = string.Join( ":", m_group, m_resource, permissionsString );
 			return result;
+		}
+
+		public override bool Equals( object obj ) {
+			if( ReferenceEquals( this, obj ) ) {
+				return true;
+			}
+
+			var otherScope = obj as Scope;
+			if( otherScope == null ) {
+				return false;
+			}
+
+			return this.GetHashCode() == otherScope.GetHashCode();
+		}
+
+		public override int GetHashCode() {
+			return this.ToString().GetHashCode();
+		}
+
+		public static bool TryParse( string input, out Scope scope ) {
+			string[] parts = input.Split( ':' );
+			if( parts.Length != 3 ) {
+				scope = null;
+				return false;
+			}
+
+			string group = parts[0];
+			string resource = parts[1];
+			string[] permissions = parts[2].Split( ',' );
+
+			scope = new Scope( group, resource, permissions );
+			return true;
 		}
 
 	}
