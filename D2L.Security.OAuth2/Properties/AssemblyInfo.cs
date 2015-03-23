8db74f9784f55076b3cddf2212e809c67cb63fe7
Owen Smith <owen.smith@d2l.com>
2015-03-23 14:31:16 -0400

    chore(nuget) configure nuget packaging

--- D2L.Security.OAuth2/Properties/AssemblyInfo.cs
+++ D2L.Security.OAuth2/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("D2L.Security.OAuth2")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library for interacting with OAuth2 based flows in D2L systems")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Desire2Learn")]
 [assembly: AssemblyProduct("D2L.Security.OAuth2")]
