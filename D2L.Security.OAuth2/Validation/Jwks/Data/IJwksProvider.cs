48420505e56e1d1a22ef1603a9df6da901fed260
Brian Pearson <Brian.Pearson@d2l.com>
2015-03-31 13:56:27 -0400

    US50139 - token validation caching, work in progress

---
+++ D2L.Security.OAuth2/Validation/Jwks/Data/IJwksProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D2L.Security.OAuth2.Validation.Jwks.Data {
+	internal interface IJwksProvider {
+		Task<string> RequestJwksAsync( Uri jwksEndpoint );
+	}
+}
