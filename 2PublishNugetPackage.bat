5fac340c6f188247348b9a97bd25657f5c193c04
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-04-15 18:09:55 -0400

    Nuget version 2.0.1.0

--- 2PublishNugetPackage.bat
+++ 2PublishNugetPackage.bat
@@ -1,3 +1,3 @@
-..\.nuget\nuget push D2L.Security.OAuth2.2.0.0.0.nupkg 88113573-2f5d-4c30-969a-46f32f2f4e9c -s http://nuget.build.d2l/nuget/stable
+.nuget\nuget push D2L.Security.OAuth2.2.0.1.0.nupkg 6E553704-38F4-4543-85EF-D3D6D992A8A0 -s http://nuget.build.d2l/nuget/stable
 
 pause
