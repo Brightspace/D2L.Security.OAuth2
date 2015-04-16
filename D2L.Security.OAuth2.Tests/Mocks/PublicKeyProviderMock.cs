4f5eeb8fe6a6a221363ea2b3de80baa918cf7f11
Brian Pearson <Brian.Pearson@d2l.com>
2015-04-16 18:21:48 -0400

    US50139 - tests for access token validator

---
+++ D2L.Security.OAuth2.Tests/Mocks/PublicKeyProviderMock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D2L.Security.OAuth2.SecurityTokens;
+using D2L.Security.OAuth2.Validation.Jwks;
+using Moq;
+
+namespace D2L.Security.OAuth2.Tests.Mocks {
+	public static class PublicKeyProviderMock {
+
+		internal static Mock<IPublicKeyProvider> Create(
+			Uri jwksEndpoint,
+			string keyId,
+			D2LSecurityToken token
+		) {
+
+			var mock = new Mock<IPublicKeyProvider>();
+
+			mock.Setup( p => p.GetSecurityTokenAsync(
+				jwksEndpoint,
+				keyId
+			)).Returns( Task.FromResult( token ) );
+
+			return mock;
+
+		}
+
+	}
+}
