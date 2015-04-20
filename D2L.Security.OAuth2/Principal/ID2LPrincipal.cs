d8af7fbfa63d02b38e3b4842e1d666b1a4310568
Brian Pearson <Brian.Pearson@d2l.com>
2015-04-20 17:52:33 -0400

    US50928 - added test for anonymous principal type.  document principal properties.  change tenantid to guid

--- D2L.Security.OAuth2/Principal/ID2LPrincipal.cs
+++ D2L.Security.OAuth2/Principal/ID2LPrincipal.cs
@@ -1,12 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using D2L.Security.OAuth2.Scopes;
 using D2L.Security.OAuth2.Validation.AccessTokens;
 
 namespace D2L.Security.OAuth2.Principal {
 	public interface ID2LPrincipal {
 
+		/// <summary>
+		/// Only valid if the PrincipalType is User
+		/// </summary>
 		string UserId { get; }
-		string TenantId { get; }
+
+		/// <summary>
+		/// Only valid if the PrincipalType is User or Service
+		/// </summary>
+		Guid TenantId { get; }
 
 		PrincipalType Type { get; }
 		IEnumerable<Scope> Scopes { get; }
