c76ba8d141986d5889af3aa67d1a49eee0590ff7
Jacob Parker <Jacob.Parker@D2L.com>
2015-04-21 11:29:14 -0400

    US50230 - Move issuer into UnsignedToken

--- D2L.Security.OAuth2/Keys/Local/UnsignedToken.cs
+++ D2L.Security.OAuth2/Keys/Local/UnsignedToken.cs
@@ -5,21 +5,28 @@ using System.Security.Claims;
 
 namespace D2L.Security.OAuth2.Keys.Local {
 	public sealed class UnsignedToken {
+		private readonly string m_issuer;
 		private readonly string m_audience;
 		private readonly ReadOnlyCollection<Claim> m_claims;
 		private readonly DateTime m_notBefore;
 		private readonly DateTime m_expiresAt;
 
 		public UnsignedToken(
+			string issuer,
 			string audience,
 			IList<Claim> claims,
 			DateTime notBefore,
 			DateTime expiresAt
 		) {
+			m_issuer = issuer;
 			m_audience = audience;
 			m_claims = new ReadOnlyCollection<Claim>( claims );
 			m_notBefore = notBefore;
 			m_expiresAt = expiresAt;
+		}
+
+		public string Issuer {
+			get { return m_issuer; }
 		}
 
 		public string Audience {
