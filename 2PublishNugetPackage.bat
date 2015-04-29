5621c118248a327039ea2eaac56589b5a7d2d023
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-04-29 16:11:53 -0400

    2.2.6

--- 2PublishNugetPackage.bat
+++ 2PublishNugetPackage.bat
@@ -1,3 +1,3 @@
-.nuget\nuget push D2L.Security.OAuth2.2.2.5.0.symbols.nupkg REDACTED-nuget-publish-key -s http://nuget.build.d2l/nuget/stable
+.nuget\nuget push D2L.Security.OAuth2.2.2.6.0.symbols.nupkg REDACTED-nuget-publish-key -s http://nuget.build.d2l/nuget/stable
 
 pause
