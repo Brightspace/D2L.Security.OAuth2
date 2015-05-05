5b1a87ea76968c9a3e962e7d720f5d30fcea9dae
Jacob Parker <Jacob.Parker@D2L.com>
2015-05-05 14:32:50 -0400

    Add IEnumerableClaim extension methods

---
+++ D2L.Security.OAuth2/Utilities/IEnumerableClaimExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace D2L {
+	public static class IEnumerableClaimExtensions {
+		public static bool HasClaim( this IEnumerable<Claim> @this, string name ) {
+			return @this.Select( c => c.Type == name ).Any();
+		}
+
+		public static bool TryGetClaim( this IEnumerable<Claim> @this, string name, out string value ) {
+			Claim claim = @this.FirstOrDefault( c => c.Type == name );
+
+			if( claim == null ) {
+				value = null;
+				return false;
+			}
+
+			value = claim.Value;
+			return true;
+		}
+	}
+}
