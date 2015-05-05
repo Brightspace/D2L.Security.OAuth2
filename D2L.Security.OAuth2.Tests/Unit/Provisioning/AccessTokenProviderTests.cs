daf42717733cee147ede2dbf4a772ca41a091143
Jacob Parker <Jacob.Parker@D2L.com>
2015-05-05 14:19:16 -0400

    Take IKeyManager in IAccessTokenProviders constructor

---
+++ D2L.Security.OAuth2.Tests/Unit/Provisioning/AccessTokenProviderTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Security.Claims;
+
+using D2L.Security.OAuth2.Keys.Local;
+using D2L.Security.OAuth2.Keys.Local.Data;
+using D2L.Security.OAuth2.Provisioning;
+using D2L.Security.OAuth2.Provisioning.Default;
+using D2L.Security.OAuth2.Scopes;
+
+using Moq;
+
+using NUnit.Framework;
+
+namespace D2L.Security.OAuth2.Tests.Unit.Provisioning {
+	
+	[TestFixture]
+	internal sealed partial class AccessTokenProviderTests {
+
+		private static class TestData {
+			public const string ISSUER = "someIssuer";
+			public const string TENANT_ID = "someTenant";
+			public const string USER = "someUser";
+			public const string XSRF_TOKEN = "someXsrfToken";
+		}
+
+		[Test]
+		public async void ProvisionAccessTokenAsync_AssertionTokenIsSigned() {
+			byte[] privateKey;
+			byte[] publicKey;
+			Guid keyId;
+			MakeKeyPair( out privateKey, out publicKey, out keyId );
+
+			string actualAssertion = null;
+			Mock<IAuthServiceClient> clientMock = new Mock<IAuthServiceClient>();
+			clientMock
+				.Setup( x => x.ProvisionAccessTokenAsync( It.IsAny<string>(), It.IsAny<IEnumerable<Scope>>() ) )
+				.Callback<string, IEnumerable<Scope>>( ( assertion, _ ) => actualAssertion = assertion )
+				.ReturnsAsync( null );
+
+#pragma warning disable 618
+			IKeyManager keyManager = KeyManagerFactory.Create( new InMemoryPublicKeyDataProvider() );
+#pragma warning restore 618
+
+			IAccessTokenProvider provider = new AccessTokenProvider( TestData.ISSUER, keyManager, clientMock.Object );
+
+			var claims = new List<Claim>{
+				new Claim( Constants.Claims.TENANT_ID, TestData.TENANT_ID ),
+				new Claim( Constants.Claims.USER_ID, TestData.USER ),
+				new Claim( Constants.Claims.XSRF_TOKEN, TestData.XSRF_TOKEN )
+			};
+
+			var scopes = new Scope[] { };
+
+			await provider.ProvisionAccessTokenAsync( claims, scopes ).SafeAsync();
+
+			var tokenHandler = new JwtSecurityTokenHandler();
+			var unvalidatedToken = (JwtSecurityToken)tokenHandler.ReadToken( actualAssertion );
+
+			var publicKeys = (await keyManager.GetAllAsync().SafeAsync()).ToList();
+
+			string expectedKeyId = publicKeys.First().Id.ToString();
+			string actualKeyId = unvalidatedToken.Header.SigningKeyIdentifier[ 0 ].Id;
+
+			Assert.AreEqual( 1, publicKeys.Count );
+			Assert.AreEqual( TestData.ISSUER, unvalidatedToken.Issuer );
+			Assert.AreEqual( expectedKeyId, actualKeyId );
+
+			AssertClaimEquals( unvalidatedToken, Constants.Claims.TENANT_ID, TestData.TENANT_ID );
+			AssertClaimEquals( unvalidatedToken, Constants.Claims.USER_ID, TestData.USER );
+			AssertClaimEquals( unvalidatedToken, Constants.Claims.XSRF_TOKEN, TestData.XSRF_TOKEN );
+		}
+
+		private void AssertClaimEquals( JwtSecurityToken token, string name, string value ) {
+			Claim claim = token.Claims.FirstOrDefault( c => c.Type == name );
+			Assert.IsNotNull( claim );
+			Assert.AreEqual( value, claim.Value );
+		}
+	}
+}
---
+++ D2L.Security.OAuth2/Provisioning/Default/AccessTokenProvider.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+using D2L.Security.OAuth2.Keys.Local;
+using D2L.Security.OAuth2.Scopes;
+
+namespace D2L.Security.OAuth2.Provisioning.Default {
+	public sealed class AccessTokenProvider : IAccessTokenProvider {
+		private readonly string m_issuer;
+		private readonly IAuthServiceClient m_client;
+		private readonly IKeyManager m_keyManager;
+		private readonly bool m_disposeOfClient;
+
+		public AccessTokenProvider(
+			string issuer,
+			IKeyManager keyManager,
+			IAuthServiceClient authServiceClient,
+			bool disposeOfClient = true
+		) {
+			m_issuer = issuer;
+			m_keyManager = keyManager;
+			m_client = authServiceClient;
+			m_disposeOfClient = disposeOfClient;
+		}
+
+		async Task<IAccessToken> IAccessTokenProvider.ProvisionAccessTokenAsync(
+			IEnumerable<Claim> claimSet,
+			IEnumerable<Scope> scopes
+		) {
+			scopes = scopes ?? Enumerable.Empty<Scope>();
+
+			DateTime now = DateTime.UtcNow;
+
+			var unsignedToken = new UnsignedToken(
+				issuer: m_issuer,
+				audience: ProvisioningConstants.AssertionGrant.AUDIENCE,
+				claims: claimSet.ToList(),
+				notBefore: now,
+				expiresAt: now + ProvisioningConstants.AssertionGrant.ASSERTION_TOKEN_LIFETIME );
+
+			string assertion = await m_keyManager
+				.SignAsync( unsignedToken )
+				.SafeAsync();
+
+			return await m_client
+				.ProvisionAccessTokenAsync( assertion, scopes )
+				.SafeAsync();
+		}
+
+		public void Dispose() {
+			if( m_disposeOfClient ) {
+				m_client.Dispose();
+			}
+		}
+	}
+}
