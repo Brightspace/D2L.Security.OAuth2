f21c58ed35017df357125c2e4f4517803427558e
Jacob Parker <Jacob.Parker@D2L.com>
2015-05-06 10:52:38 -0400

    Bump version number

--- 2PublishNugetPackage.bat
+++ 2PublishNugetPackage.bat
@@ -1,3 +1,3 @@
-.nuget\nuget push D2L.Security.OAuth2.2.2.4.0.symbols.nupkg REDACTED-nuget-publish-key -s http://nuget.build.d2l/nuget/stable
+.nuget\nuget push D2L.Security.OAuth2.2.3.0.0.symbols.nupkg REDACTED-nuget-publish-key -s http://nuget.build.d2l/nuget/stable
 
 pause
