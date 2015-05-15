33c230ee5087da68e7681f7ffec323e927beb157
Jacob Parker <Jacob.Parker@D2L.com>
2015-05-15 12:08:52 -0400

    US52987 - IPublicKeyProvider.GetByIdAsync should clean up any expired keys

--- D2L.Security.OAuth2.Tests/Unit/Keys/Local/PublicKeyProviderTests.cs
+++ D2L.Security.OAuth2.Tests/Unit/Keys/Local/PublicKeyProviderTests.cs
@@ -19,21 +19,31 @@ namespace D2L.Security.OAuth2.Tests.Unit.Keys.Local {
 	internal sealed class PublicKeyProviderTests {
 		private Mock<IPublicKeyDataProvider> m_mockPublicKeyDataProvider;
 		private IPublicKeyProvider m_publicKeyProvider;
+		private List<JsonWebKey> m_allTheKeys;
 
 		[SetUp]
 		public void SetUp() {
 			m_mockPublicKeyDataProvider = new Mock<IPublicKeyDataProvider>( MockBehavior.Strict );
 
+			m_allTheKeys = new List<JsonWebKey>();
+
 			m_publicKeyProvider = new PublicKeyProvider(
 				m_mockPublicKeyDataProvider.Object,
 				JsonWebKeyStub.KEY_LIFETIME );
+
+			m_mockPublicKeyDataProvider
+				.Setup( pkdp => pkdp.GetAllAsync() )
+				.ReturnsAsync( m_allTheKeys );
 		}
 
 		[Test]
 		public async Task GetByIdAsync_EmptyDb_ReturnsNull() {
-			m_mockPublicKeyDataProvider.Setup( kp => kp.GetByIdAsync( It.IsAny<Guid>() ) ).ReturnsAsync( null );
+			Guid id = Guid.NewGuid();
+			m_mockPublicKeyDataProvider.Setup( kp => kp.GetByIdAsync( id ) ).ReturnsAsync( null );
 
-			JsonWebKey result = await m_publicKeyProvider.GetByIdAsync( Guid.NewGuid() );
+			JsonWebKey result = await m_publicKeyProvider
+				.GetByIdAsync( id )
+				.SafeAsync();
 
 			Assert.IsNull( result );
 		}
@@ -41,10 +51,11 @@ namespace D2L.Security.OAuth2.Tests.Unit.Keys.Local {
 		[Test]
 		public async Task GetByIdAsync_InvalidId_ReturnsNull() {
 			JsonWebKey key = new JsonWebKeyStub( Guid.NewGuid() );
-			m_mockPublicKeyDataProvider.Setup( kp => kp.GetByIdAsync( key.Id ) ).ReturnsAsync( key );
-			m_mockPublicKeyDataProvider.Setup( kp => kp.GetByIdAsync( It.IsAny<Guid>() ) ).ReturnsAsync( null );
+			AddKeyToDb( key );
 
-			JsonWebKey result = await m_publicKeyProvider.GetByIdAsync( Guid.NewGuid() );
+			JsonWebKey result = await m_publicKeyProvider
+				.GetByIdAsync( Guid.NewGuid() )
+				.SafeAsync();
 
 			Assert.IsNull( result );
 		}
@@ -52,9 +63,11 @@ namespace D2L.Security.OAuth2.Tests.Unit.Keys.Local {
 		[Test]
 		public async Task GetByIdAsync_ValidId_ReturnsKey() {
 			JsonWebKey key = new JsonWebKeyStub( Guid.NewGuid() );
-			m_mockPublicKeyDataProvider.Setup( kp => kp.GetByIdAsync( key.Id ) ).ReturnsAsync( key );
+			AddKeyToDb( key );
 
-			JsonWebKey result = await m_publicKeyProvider.GetByIdAsync( key.Id );
+			JsonWebKey result = await m_publicKeyProvider
+				.GetByIdAsync( key.Id )
+				.SafeAsync();
 
 			Assert.IsNotNull( result );
 			Assert.AreEqual( key.Id, result.Id );
@@ -63,10 +76,12 @@ namespace D2L.Security.OAuth2.Tests.Unit.Keys.Local {
 		[Test]
 		public async Task GetByIdAsync_ExpiredKey_DeletesAndReturnsNull() {
 			JsonWebKey key = new JsonWebKeyStub( Guid.NewGuid(), DateTime.UtcNow - TimeSpan.FromMinutes( 1 ) );
-			m_mockPublicKeyDataProvider.Setup( kp => kp.GetByIdAsync( key.Id ) ).ReturnsAsync( key );
+			AddKeyToDb( key );
 			m_mockPublicKeyDataProvider.Setup( kp => kp.DeleteAsync( key.Id ) ).Returns( Task.Delay( 0 ) );
 
-			JsonWebKey result = await m_publicKeyProvider.GetByIdAsync( key.Id );
+			JsonWebKey result = await m_publicKeyProvider
+				.GetByIdAsync( key.Id )
+				.SafeAsync();
 
 			m_mockPublicKeyDataProvider.Verify( kp => kp.DeleteAsync( key.Id ), Times.Once() );
 
@@ -74,9 +89,34 @@ namespace D2L.Security.OAuth2.Tests.Unit.Keys.Local {
 		}
 
 		[Test]
+		public async Task GetByIdAsync_OtherKeyExpired_GetsDeletedAsWell() {
+			JsonWebKey freshKey = new JsonWebKeyStub( Guid.NewGuid() );
+			JsonWebKey expiredKey = new JsonWebKeyStub( Guid.NewGuid(), DateTime.UtcNow - TimeSpan.FromSeconds( 1 ) );
+			AddKeyToDb( freshKey );
+			AddKeyToDb( expiredKey );
+			m_mockPublicKeyDataProvider
+				.Setup( pkdb => pkdb.DeleteAsync( expiredKey.Id ) )
+				.Returns( Task.Delay( 0 ) );
+
+			JsonWebKey result = await m_publicKeyProvider
+				.GetByIdAsync( freshKey.Id )
+				.SafeAsync();
+
+			Assert.IsNotNull( result );
+			Assert.AreEqual( freshKey.Id, result.Id );
+
+			m_mockPublicKeyDataProvider.Verify( pkdp => pkdp.DeleteAsync( expiredKey.Id ), Times.Once );
+		}
+
+		[Test]
 		public async Task GetAll_EmptyDb_ReturnsEmptySet() {
-			m_mockPublicKeyDataProvider.Setup( kp => kp.GetAllAsync() ).ReturnsAsync( Enumerable.Empty<JsonWebKey>() );
-			IEnumerable<JsonWebKey> result = await m_publicKeyProvider.GetAllAsync();
+			m_mockPublicKeyDataProvider
+				.Setup( kp => kp.GetAllAsync() )
+				.ReturnsAsync( Enumerable.Empty<JsonWebKey>() );
+
+			IEnumerable<JsonWebKey> result = await m_publicKeyProvider
+				.GetAllAsync()
+				.SafeAsync();
 
 			Assert.IsEmpty( result );
 		}
@@ -93,13 +133,22 @@ namespace D2L.Security.OAuth2.Tests.Unit.Keys.Local {
 				new JsonWebKeyStub( goodIds[ 2 ] ),
 				new JsonWebKeyStub( mehIds[ 2 ], DateTime.UtcNow - TimeSpan.FromMilliseconds( 1 ) )
 			};
-			m_mockPublicKeyDataProvider.Setup( kp => kp.GetAllAsync() ).ReturnsAsync( keys );
+
+			m_mockPublicKeyDataProvider
+				.Setup( kp => kp.GetAllAsync() )
+				.ReturnsAsync( keys );
+
 			foreach( var id in mehIds ) {
 				var kid = id; // copy the GUID to appease the compiler
-				m_mockPublicKeyDataProvider.Setup( kp => kp.DeleteAsync( kid ) ).Returns( Task.Delay( 0 ) );
+				m_mockPublicKeyDataProvider
+					.Setup( kp => kp.DeleteAsync( kid ) )
+					.Returns( Task.Delay( 0 ) );
 			}
 
-			IEnumerable<JsonWebKey> result = await m_publicKeyProvider.GetAllAsync();
+			IEnumerable<JsonWebKey> result = await m_publicKeyProvider
+				.GetAllAsync()
+				.SafeAsync();
+
 			IEnumerable<Guid> ids = result.Select( k => k.Id );
 
 			foreach( var id in mehIds ) {
@@ -108,6 +157,13 @@ namespace D2L.Security.OAuth2.Tests.Unit.Keys.Local {
 			}
 
 			CollectionAssert.AreEquivalent( goodIds, ids );
+		}
+
+		private void AddKeyToDb( JsonWebKey key ) {
+			m_allTheKeys.Add( key );
+			m_mockPublicKeyDataProvider
+				.Setup( pkdp => pkdp.GetByIdAsync( key.Id ) )
+				.ReturnsAsync( key );
 		}
 	}
 }
