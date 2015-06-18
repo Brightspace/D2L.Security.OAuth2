a8417a00d6e0e0c39ede8f1bd8a72603e968ce7a
Dave Fallis <Dave.Fallis@d2l.com>
2015-06-18 13:10:30 -0400

    US50118 Moved caching from the constructor to the method invocation to avoid async issues with missing http context in the LMS

--- D2L.Security.OAuth2/Provisioning/AccessTokenProviderFactory.cs
+++ D2L.Security.OAuth2/Provisioning/AccessTokenProviderFactory.cs
@@ -12,32 +12,19 @@ namespace D2L.Security.OAuth2.Provisioning {
 
 		/// <summary>
 		/// Factory method for creating new <see cref="IAccessTokenProvider"/> instances.
-		/// Any provided <see cref="ICache"/> instances do not need to check for token 
-		/// expiration or grace period because the <see cref="IAccessTokenProvider"/> 
-		/// will handle it internally.
 		/// </summary>
 		/// <returns>A new <see cref="IAccessTokenProvider"/></returns>
 		public static IAccessTokenProvider Create(
 			IKeyManager keyManager,
 			IAuthServiceClient authServiceClient,
 			TimeSpan tokenRefreshGracePeriod,
-			ICache userTokenCache = null,
-			ICache serviceTokenCache = null,
 			bool disposeOfClient = true
 			) {
-
-			if( userTokenCache == null ) {
-				userTokenCache = new NullCache();
-			}
-
-			if( serviceTokenCache == null ) {
-				serviceTokenCache = new NullCache();
-			}
 
 			IAccessTokenProvider accessTokenProvider =
 				new AccessTokenProvider( keyManager, authServiceClient, disposeOfClient );
 
-			return new CachedAccessTokenProvider( accessTokenProvider, userTokenCache, serviceTokenCache, tokenRefreshGracePeriod );
+			return new CachedAccessTokenProvider( accessTokenProvider, tokenRefreshGracePeriod );
 		}
 	}
 }
