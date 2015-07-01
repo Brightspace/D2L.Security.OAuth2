9a7da9365f632c79bb1c07743d01b289d3f19106
Owen Smith <Owen.Smith@d2l.com>
2015-07-01 11:59:36 -0400

    (bench) add initial benchmark setup. bench full-stack token validation
    Added this to give visibility into changes I was making for token validation,
    as such I've only added a validation bench.
    
    Things like signing would good, as well as, perhaps, lower-level validation
    (to give compute baselines)

---
+++ D2L.Security.OAuth2.Benchmarks/FullStackValidation/FullStackValidationBenchmarks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BenchmarkIt;
+
+namespace D2L.Security.OAuth2.Benchmarks.FullStackValidation {
+	internal sealed class FullStackValidationBenchmarks {
+
+		private const int WARMUP_ITERATIONS = 1000;
+		private static TimeSpan RUNTIME = TimeSpan.FromSeconds( 4 );
+
+		public static void Run() {
+			Action RS256 = ( (IBenchmark)new RS256() ).GetRunner();
+
+			Console.WriteLine( typeof( FullStackValidationBenchmarks ).Name );
+			Console.WriteLine( string.Format( "Warmup: {0} iterations", WARMUP_ITERATIONS ) );
+			Console.WriteLine( string.Format( "Runtime: {0} seconds/benchmark", RUNTIME.Seconds ) );
+			Console.WriteLine();
+
+			Benchmark
+				.This( "RS256", RS256 )
+				.WithWarmup( WARMUP_ITERATIONS )
+				.For( RUNTIME.Seconds )
+				.Seconds()
+				.PrintComparison();
+		}
+
+	}
+}
