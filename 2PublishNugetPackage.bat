d8f996982c628b39afc70e991136c8eda77210e2
Dave Fallis <Dave.Fallis@d2l.com>
2015-07-08 09:23:22 -0400

    US54677 Remove ability to store private key components in public key

--- 2PublishNugetPackage.bat
+++ 2PublishNugetPackage.bat
@@ -1,2 +1,2 @@
-.nuget\nuget push D2L.Security.OAuth2.2.6.0.0.symbols.nupkg 6E553704-38F4-4543-85EF-D3D6D992A8A0 -s http://nuget.build.d2l/nuget/stable
+.nuget\nuget push D2L.Security.OAuth2.2.7.0.0.symbols.nupkg 6E553704-38F4-4543-85EF-D3D6D992A8A0 -s http://nuget.build.d2l/nuget/stable
 pause
