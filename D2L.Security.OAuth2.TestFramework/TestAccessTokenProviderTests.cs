3066b3a7aedf6b45598e2e44171c5904b0a262ca
Alex Bedley <alex.bedley@D2L.com>
2015-07-08 15:05:42 -0400

    TestFramework - initial attempt

---
+++ D2L.Security.OAuth2.TestFramework/TestAccessTokenProviderTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using D2L.Security.OAuth2.Provisioning;
+using D2L.Security.OAuth2.Scopes;
+using NUnit.Framework;
+
+namespace D2L.Security.OAuth2.TestFramework {
+
+	[TestFixture]
+	internal sealed class TestAccessTokenProviderTests {
+
+		[Test]
+		public async void TestTestAccessTokenProvider()
+		{
+			IAccessTokenProvider provider = TestAccessTokenProviderFactory.Create("https://auth-dev.proddev.d2l/core/");
+
+			IAccessToken token = await provider.ProvisionAccessTokenAsync(new ClaimSet("ur mom"), new Scope[] { new Scope("*", "*", "*") });
+
+			Console.WriteLine(token.Token);
+		}
+	}
+}
