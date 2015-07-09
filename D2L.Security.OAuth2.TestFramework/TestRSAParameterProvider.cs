7b1ce0e15c61cbcb72f30163294fe463c737d1a8
Alex Bedley <alex.bedley@D2L.com>
2015-07-09 15:01:29 -0400

    TestFramework - dispose of RSA csp

--- D2L.Security.OAuth2.TestFramework/TestRSAParameterProvider.cs
+++ D2L.Security.OAuth2.TestFramework/TestRSAParameterProvider.cs
@@ -8,10 +8,12 @@ namespace D2L.Security.OAuth2.TestFramework {
 		static private readonly RSAParameters m_parameters;
 
 		static TestRSAParameterProvider() {
-			var provider = new RSACryptoServiceProvider( 2048 ) { PersistKeyInCsp = false };
-			provider.FromXmlString( Resources.RSAParameters );
+			using (var provider = new RSACryptoServiceProvider(2048) {PersistKeyInCsp = false})
+			{
+				provider.FromXmlString( Resources.RSAParameters );
 
-			m_parameters = provider.ExportParameters( true );
+				m_parameters = provider.ExportParameters( true );
+			}
 		}
 
 		public static RSAParameters RSAParameters {
