58967b1c41c594a416e6039b246a98473ef8fb05
Alex Bedley <alex.bedley@D2L.com>
2015-07-09 15:18:05 -0400

    TestFramework - further refactoring

--- D2L.Security.OAuth2.TestFramework/TestAccessTokenProviderFactory.cs
+++ D2L.Security.OAuth2.TestFramework/TestAccessTokenProviderFactory.cs
@@ -8,24 +8,20 @@ using D2L.Security.OAuth2.Keys.Development;
 using D2L.Security.OAuth2.Provisioning;
 using D2L.Security.OAuth2.Provisioning.Default;
 
-namespace D2L.Security.OAuth2.TestFramework
-{
-	public static class TestAccessTokenProviderFactory
-    {
+namespace D2L.Security.OAuth2.TestFramework {
+	public static class TestAccessTokenProviderFactory {
 		private static readonly Guid TestGuid = new Guid( "FA7C07A8-42C8-4C57-9AF2-CCE10C271033" );
 
-		public static IAccessTokenProvider Create(HttpClient httpClient, String tokenProvisioningEndpoint)
-		{
-			return Create(httpClient, tokenProvisioningEndpoint, TestGuid, TestRSAParametersProvider.TestRSAParameters);
+		public static IAccessTokenProvider Create( HttpClient httpClient, String tokenProvisioningEndpoint ) {
+			return Create( httpClient, tokenProvisioningEndpoint, TestGuid, TestRSAParametersProvider.TestRSAParameters );
 		}
 
-		public static IAccessTokenProvider Create(HttpClient httpClient, String tokenProvisioningEndpoint, Guid guid, RSAParameters rsaParameters)
-		{
-			IAuthServiceClient authServiceClient = new AuthServiceClient( httpClient, new Uri( tokenProvisioningEndpoint ) );
+		public static IAccessTokenProvider Create( HttpClient httpClient, String tokenProvisioningEndpoint, Guid guid, RSAParameters rsaParameters ) {
 #pragma warning disable 618
 			IPrivateKeyProvider privateKeyProvider = new StaticPrivateKeyProvider( guid, rsaParameters );
 #pragma warning restore 618
 			ITokenSigner tokenSigner = new TokenSigner( privateKeyProvider );
+			IAuthServiceClient authServiceClient = new AuthServiceClient( httpClient, new Uri( tokenProvisioningEndpoint ) );
 			INonCachingAccessTokenProvider noCacheTokenProvider = new AccessTokenProvider( tokenSigner, authServiceClient );
 
 			return new CachedAccessTokenProvider( noCacheTokenProvider, Timeout.InfiniteTimeSpan );
--- D2L.Security.OAuth2.TestFramework/TestRSAParametersProvider.cs
+++ D2L.Security.OAuth2.TestFramework/TestRSAParametersProvider.cs
@@ -8,10 +8,8 @@ namespace D2L.Security.OAuth2.TestFramework {
 		static private readonly RSAParameters m_parameters;
 
 		static TestRSAParametersProvider() {
-			using (var provider = new RSACryptoServiceProvider(2048) {PersistKeyInCsp = false})
-			{
+			using (var provider = new RSACryptoServiceProvider { PersistKeyInCsp = false }) {
 				provider.FromXmlString( Resources.TestRSAParameters );
-
 				m_parameters = provider.ExportParameters( true );
 			}
 		}
