57e46d1b5baba3832720f40a8e02f4ec8a80791d
Alex Bedley <alex.bedley@D2L.com>
2015-07-09 14:57:01 -0400

    TestFramework - read rsa params from file

--- D2L.Security.OAuth2.TestFramework/TestAccessTokenProviderTests.cs
+++ D2L.Security.OAuth2.TestFramework/TestAccessTokenProviderTests.cs
@@ -11,15 +11,15 @@ namespace D2L.Security.OAuth2.TestFramework {
 	[TestFixture]
 	internal sealed class TestAccessTokenProviderTests {
 
-		private const string AUTH_URL = "https://auth-dev.proddev.d2l/core";
+		private const string DEV_AUTH_URL = "https://auth-dev.proddev.d2l/core";
 
 		[Test]
 		public async void TestTestAccessTokenProvider() {
 			using (var httpClient = new HttpClient()) {
-				IAccessTokenProvider provider = TestAccessTokenProviderFactory.Create( httpClient, AUTH_URL );
+				IAccessTokenProvider provider = TestAccessTokenProviderFactory.Create( httpClient, DEV_AUTH_URL );
 				IAccessToken token = await provider.ProvisionAccessTokenAsync( new ClaimSet( "ExpandoClient", Guid.NewGuid() ), new[] { new Scope( "*", "*", "*" ) } );
 
-				IAccessTokenValidator validator = AccessTokenValidatorFactory.CreateRemoteValidator( httpClient, new Uri( AUTH_URL ) );
+				IAccessTokenValidator validator = AccessTokenValidatorFactory.CreateRemoteValidator( httpClient, new Uri( DEV_AUTH_URL ) );
 				var result = await validator.ValidateAsync(token.Token);
 
 				Assert.AreEqual(result.Status, ValidationStatus.Success);
---
+++ D2L.Security.OAuth2.TestFramework/TestRSAParameterProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using D2L.Security.OAuth2.TestFramework.Properties;
+
+namespace D2L.Security.OAuth2.TestFramework {
+
+	internal static class TestRSAParameterProvider {
+
+		static private readonly RSAParameters m_parameters;
+
+		static TestRSAParameterProvider() {
+			var provider = new RSACryptoServiceProvider( 2048 ) { PersistKeyInCsp = false };
+			provider.FromXmlString( Resources.RSAParameters );
+
+			m_parameters = provider.ExportParameters( true );
+		}
+
+		public static RSAParameters RSAParameters {
+			get {
+				return m_parameters;
+			}
+		}
+	}
+}
