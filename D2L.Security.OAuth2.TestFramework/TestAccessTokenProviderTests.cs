62f2689f6003e6af4831442a81610fb117eb57aa
Alex Bedley <alex.bedley@D2L.com>
2015-07-09 13:34:53 -0400

    TestFramework respond to changes after refactor

--- D2L.Security.OAuth2.TestFramework/TestAccessTokenProviderTests.cs
+++ D2L.Security.OAuth2.TestFramework/TestAccessTokenProviderTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http;
 using D2L.Security.OAuth2.Provisioning;
 using D2L.Security.OAuth2.Scopes;
 using NUnit.Framework;
@@ -11,11 +12,15 @@ namespace D2L.Security.OAuth2.TestFramework {
 		[Test]
 		public async void TestTestAccessTokenProvider()
 		{
-			IAccessTokenProvider provider = TestAccessTokenProviderFactory.Create("https://auth-dev.proddev.d2l/core/connect/token");
+			using (var httpClient = new HttpClient())
+			{
+				IAccessTokenProvider provider = TestAccessTokenProviderFactory.Create(httpClient, "https://auth-dev.proddev.d2l/core" );
 
-			IAccessToken token = await provider.ProvisionAccessTokenAsync( new ClaimSet( "ExpandoClient", Guid.NewGuid() ), new[] { new Scope( "*", "*", "*" ) } );
+				IAccessToken token = await provider.ProvisionAccessTokenAsync( new ClaimSet( "ExpandoClient", Guid.NewGuid() ), new[] { new Scope( "*", "*", "*" ) } );
 
-			Console.WriteLine(token.Token);
+				Console.WriteLine( token.Token );
+			}
+			
 		}
 	}
 }
