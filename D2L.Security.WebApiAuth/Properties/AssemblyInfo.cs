bf83df69795145fe76419d8396567e5614d32e4d
Sebastian Mihai <Sebastian.Mihai@d2l.com>
2015-07-14 17:25:21 -0400

    Version 2.4

--- D2L.Security.WebApiAuth/Properties/AssemblyInfo.cs
+++ D2L.Security.WebApiAuth/Properties/AssemblyInfo.cs
@@ -13,8 +13,8 @@
 [assembly: AssemblyProduct( "Brightspace" )]
 
 // Nuget: Version
-[assembly: AssemblyInformationalVersion( "2.3.0.0" )]
-[assembly: AssemblyVersion( "2.3.0.0" )]
-[assembly: AssemblyFileVersion( "2.3.0.0" )]
+[assembly: AssemblyInformationalVersion( "2.4.0.0" )]
+[assembly: AssemblyVersion( "2.4.0.0" )]
+[assembly: AssemblyFileVersion( "2.4.0.0" )]
 
 [assembly: AssemblyCopyright( "Copyright © Desire2Learn" )]
--- D2L.Security.WebApiAuth/Publish/PublishNugetPackage.bat
+++ D2L.Security.WebApiAuth/Publish/PublishNugetPackage.bat
@@ -1,4 +1,4 @@
 
-..\..\.nuget\nuget push D2L.Security.WebApiAuth.2.3.0.0.nupkg REDACTED-nuget-publish-key -s http://nuget.build.d2l/nuget/stable
+..\..\.nuget\nuget push D2L.Security.WebApiAuth.2.4.0.0.nupkg REDACTED-nuget-publish-key -s http://nuget.build.d2l/nuget/stable
 
 pause
