5058d965bd9876a8f6ba654c1c9ba344204e9a75
Alex Bedley <alex.bedley@D2L.com>
2015-07-16 13:54:34 -0400

    TestFramework - make nuget scripts more generic

--- 1CreateNugetPackage.bat
+++ 1CreateNugetPackage.bat
@@ -1,3 +1,3 @@
-.nuget\nuget pack -Build -Symbols -Properties Configuration=Release D2L.Security.OAuth2/D2L.Security.OAuth2.csproj
+.nuget\nuget pack -Build -Symbols -Properties Configuration=Release %1
 
 pause
