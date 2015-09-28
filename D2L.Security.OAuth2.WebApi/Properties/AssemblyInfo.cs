f5a2211de49c37fe102344390b5e0b23f93c8c42
Jacob Parker <Jacob.Parker@D2L.com>
2015-09-28 10:30:26 -0400

    Bump version to 3.0.0

--- D2L.Security.OAuth2.WebApi/Properties/AssemblyInfo.cs
+++ D2L.Security.OAuth2.WebApi/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyProduct( "Brightspace" )]
 
 // Nuget: Version
-[assembly: AssemblyInformationalVersion( "2.4.0.0" )]
-[assembly: AssemblyVersion( "2.4.0.0" )]
-[assembly: AssemblyFileVersion( "2.4.0.0" )]
+[assembly: AssemblyInformationalVersion( "3.0.0.0" )]
+[assembly: AssemblyVersion( "3.0.0.0" )]
+[assembly: AssemblyFileVersion( "3.0.0.0" )]
 
 [assembly: AssemblyCopyright( "Copyright © Desire2Learn" )]
 
