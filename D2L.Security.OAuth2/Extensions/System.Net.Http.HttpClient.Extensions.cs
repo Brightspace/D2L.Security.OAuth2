622a524bbfd80a480dfa25044f9d624f5fcb1c5c
Jacob Parker <Jacob.Parker@D2L.com>
2015-12-01 14:52:27 -0500

    Add HttpClient.AuthenticateAs extension method

---
+++ D2L.Security.OAuth2/Extensions/System.Net.Http.HttpClient.Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using D2L.Security.OAuth2.Principal;
+
+namespace D2L {
+#pragma warning disable 1591 // "Missing XML comment for ... __D2LSecurityOAuth2_Extensions 
+	public static partial class __D2LSecurityOAuth2_Extensions {
+#pragma warning restore 1591
+		/// <summary>
+		/// Populate the Authorization header with an access token from a principal (e.g. the current user being serviced by an API.)	
+		/// </summary>
+		/// <param name="this"></param>
+		/// <param name="principal">This principals access token will be used</param>
+		public static void AuthenticateAs( this HttpClient @this, ID2LPrincipal principal ) {
+			@this.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+				scheme: "Bearer",
+				parameter: principal.AccessToken.SensitiveRawAccessToken
+			);	
+		}
+	}
+}
