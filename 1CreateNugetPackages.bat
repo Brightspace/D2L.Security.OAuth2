5e744d25ad417e101f8c84f54f5384bc331e5c13
Jacob Parker <j3parker@csclub.uwaterloo.ca>
2016-02-09 09:20:36 -0500

    Delete old nuget helper scripts

--- 1CreateNugetPackages.bat
+++
@@ -1,4 +0,0 @@
-.nuget\nuget pack -Build -Symbols -Properties Configuration=Release D2L.Security.OAuth2\D2L.Security.OAuth2.csproj
-.nuget\nuget pack -Build -Symbols -Properties Configuration=Release D2L.Security.OAuth2.TestFramework\D2L.Security.OAuth2.TestFramework.csproj
-
-pause
