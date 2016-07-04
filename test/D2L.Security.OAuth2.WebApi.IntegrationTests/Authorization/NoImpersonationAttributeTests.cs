9b19588d27f7c644729f3375bc9616ec06894c46
Jacob Parker <Jacob.Parker@D2L.com>
2016-07-04 14:24:35 +0100

    Add NoImpersonationAttribute so that special routes can deny access during impersonation

---
+++ test/D2L.Security.OAuth2.WebApi.IntegrationTests/Authorization/NoImpersonationAttributeTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using D2L.Services;
+using NUnit.Framework;
+
+namespace D2L.Security.OAuth2.Authorization {
+	[TestFixture]
+	internal sealed class NoImpersonationAttributeTests {
+		[Test]
+		public async Task ServiceToken_OK() {
+			string jwt = await TestUtilities
+				.GetAccessTokenValidForAMinute()
+				.SafeAsync();
+
+			await TestUtilities
+				.RunBasicAuthTest( "/authorization/imp", jwt, HttpStatusCode.NoContent )
+				.SafeAsync();
+		}
+
+		[Test]
+		public async Task UserToken_OK() {
+			string jwt = await TestUtilities
+				.GetAccessTokenValidForAMinute( userId: 1234 )
+				.SafeAsync();
+
+			await TestUtilities
+				.RunBasicAuthTest( "/authorization/imp", jwt, HttpStatusCode.NoContent )
+				.SafeAsync();
+		}
+
+		[Test]
+		public async Task ImpersonationToken_Forbidden() {
+			string jwt = await TestUtilities
+				.GetAccessTokenValidForAMinute( userId: 1234, actualUserId: 1235 )
+				.SafeAsync();
+
+			await TestUtilities
+				.RunBasicAuthTest( "/authorization/imp", jwt, HttpStatusCode.Forbidden )
+				.SafeAsync();
+		}
+	}
+}
--- test/D2L.Security.OAuth2.WebApi.IntegrationTests/TestWebService/Controllers/AuthorizationAttributeTestsController.cs
+++ test/D2L.Security.OAuth2.WebApi.IntegrationTests/TestWebService/Controllers/AuthorizationAttributeTestsController.cs
@@ -7,39 +7,48 @@ namespace D2L.Security.OAuth2.TestWebService.Controllers {
 	public sealed class AuthorizationAttributeTestsController : ApiController {
 		[HttpGet]
 		[Authentication( services: true )]
-		[Route("authorization/unspecifiedscope")]
+		[Route( "authorization/unspecifiedscope" )]
 		public void UnspecifiedScope() {
 			// Will crash because we forgot a [RequireScope(...)]	
 		}
 
 		[HttpGet]
-		[RequireScope("foo", "bar", "baz")]
-		[Route("authorization/unspecifiedprincipaltype")]
+		[RequireScope( "foo", "bar", "baz" )]
+		[Route( "authorization/unspecifiedprincipaltype" )]
 		public void UnspecifiedPrincipalType() {
 			// Will crash because we forgot an [AllowFrom(...)]	
 		}
 
 		[HttpGet]
-		[RequireScope("foo","bar","baz")]
+		[RequireScope( "foo", "bar", "baz" )]
 		[Authentication( users: true )]
-		[Route("authorization/basic")]
+		[Route( "authorization/basic" )]
 		public void Basic() {
-			
+
 		}
 
 		[HttpGet]
 		[NoRequiredScope]
 		[Authentication( users: true, services: true )]
-		[Route("authorization/noscope")]
+		[Route( "authorization/noscope" )]
 		public void NoScope() {
-			
+
 		}
 
 		[HttpGet]
 		[AllowAnonymous]
-		[Route("authorization/anonymous")]
+		[Route( "authorization/anonymous" )]
 		public void Anonymous() {
-			
+
+		}
+
+		[HttpGet]
+		[NoImpersonation]
+		[NoRequiredScope]
+		[Authentication( users: true, services: true )]
+		[Route( "authorization/imp" )]
+		public void Imp() {
+
 		}
 	}
 }
