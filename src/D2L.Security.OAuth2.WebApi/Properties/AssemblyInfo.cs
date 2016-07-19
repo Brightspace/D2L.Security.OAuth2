a5fcfd471c8408575dd09afb9c0775e1226b63e6
Jacob Parker <jacob@solidangle.ca>
2016-07-19 14:37:52 -0400

    Make assembly title match assembly name

--- src/D2L.Security.OAuth2.WebApi/Properties/AssemblyInfo.cs
+++ src/D2L.Security.OAuth2.WebApi/Properties/AssemblyInfo.cs
@@ -1,19 +1,10 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// Nuget: Title
-[assembly: AssemblyTitle( "D2L Security For Web API" )]
-
-// Nuget: Description
+[assembly: AssemblyTitle( "D2L.Security.WebApi" )]
 [assembly: AssemblyDescription( "A library that implements Web API components for authenticating D2L services." )]
-
-// Nuget: Author
 [assembly: AssemblyCompany( "Desire2Learn" )]
-
-// Nuget: Owners
 [assembly: AssemblyProduct( "Brightspace" )]
-
-// Nuget: Version
 [assembly: AssemblyInformationalVersion( "5.2.0.0" )]
 [assembly: AssemblyVersion( "5.2.0.0" )]
 [assembly: AssemblyFileVersion( "5.2.0.0" )]
