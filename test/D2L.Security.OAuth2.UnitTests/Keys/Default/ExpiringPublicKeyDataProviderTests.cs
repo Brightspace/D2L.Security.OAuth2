0f8861d94a3b89e2497404ad9cbc85164a91b2f5
Owen Smith <Owen.Smith@d2l.com>
2016-11-18 12:00:40 -0500

    keys: use IDateTimeProvider in ExpiringPublicKeyDataProvider
    Fixes: https://github.com/Brightspace/D2L.Security.OAuth2/issues/59

--- test/D2L.Security.OAuth2.UnitTests/Keys/Default/ExpiringPublicKeyDataProviderTests.cs
+++ test/D2L.Security.OAuth2.UnitTests/Keys/Default/ExpiringPublicKeyDataProviderTests.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using D2L.Services;
 using D2L.Security.OAuth2.TestUtilities;
+using D2L.Security.OAuth2.Utilities;
+using D2L.Services;
 using Moq;
 using NUnit.Framework;
 
 namespace D2L.Security.OAuth2.Keys.Default {
 	[TestFixture]
 	internal sealed class ExpiringPublicKeyDataProviderTests {
+
+		private static DateTime NOW = new DateTime( 2 );
+
 		private Mock<IPublicKeyDataProvider> m_mockPublicKeyDataProvider;
 		private IPublicKeyDataProvider m_publicKeyDataProvider;
 		private List<JsonWebKey> m_allTheKeys;
@@ -18,10 +22,14 @@ namespace D2L.Security.OAuth2.Keys.Default {
 		public void SetUp() {
 			m_mockPublicKeyDataProvider = new Mock<IPublicKeyDataProvider>( MockBehavior.Strict );
 
+			var mockDateTimeProvider = new Mock<IDateTimeProvider>( MockBehavior.Strict );
+			mockDateTimeProvider.SetupGet( x => x.UtcNow ).Returns( NOW );
+
 			m_allTheKeys = new List<JsonWebKey>();
 
 			m_publicKeyDataProvider = new ExpiringPublicKeyDataProvider(
-				m_mockPublicKeyDataProvider.Object
+				m_mockPublicKeyDataProvider.Object,
+				mockDateTimeProvider.Object
 			);
 
 			m_mockPublicKeyDataProvider
@@ -72,7 +80,7 @@ namespace D2L.Security.OAuth2.Keys.Default {
 
 		[Test]
 		public async Task GetByIdAsync_ExpiredKey_DeletesAndReturnsNull() {
-			JsonWebKey key = new JsonWebKeyStub( Guid.NewGuid(), DateTime.UtcNow - TimeSpan.FromMinutes( 1 ) );
+			JsonWebKey key = new JsonWebKeyStub( Guid.NewGuid(), NOW - TimeSpan.FromTicks( 1 ) );
 			AddKeyToDb( key );
 			m_mockPublicKeyDataProvider.Setup( kp => kp.DeleteAsync( key.Id ) ).Returns( Task.Delay( 0 ) );
 
@@ -88,7 +96,7 @@ namespace D2L.Security.OAuth2.Keys.Default {
 		[Test]
 		public async Task GetByIdAsync_OtherKeyExpired_GetsDeletedAsWell() {
 			JsonWebKey freshKey = new JsonWebKeyStub( Guid.NewGuid() );
-			JsonWebKey expiredKey = new JsonWebKeyStub( Guid.NewGuid(), DateTime.UtcNow - TimeSpan.FromSeconds( 1 ) );
+			JsonWebKey expiredKey = new JsonWebKeyStub( Guid.NewGuid(), NOW - TimeSpan.FromTicks( 1 ) );
 			AddKeyToDb( freshKey );
 			AddKeyToDb( expiredKey );
 			m_mockPublicKeyDataProvider
@@ -124,11 +132,11 @@ namespace D2L.Security.OAuth2.Keys.Default {
 			var mehIds = new[] { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() };
 			var keys = new[] {
 				new JsonWebKeyStub( goodIds[ 0 ] ),
-				new JsonWebKeyStub( mehIds[ 0 ], DateTime.UtcNow - TimeSpan.FromMilliseconds( 1 ) ),
-				new JsonWebKeyStub( mehIds[ 1 ], DateTime.UtcNow - TimeSpan.FromMilliseconds( 1 ) ),
+				new JsonWebKeyStub( mehIds[ 0 ], NOW - TimeSpan.FromTicks( 1 ) ),
+				new JsonWebKeyStub( mehIds[ 1 ], NOW - TimeSpan.FromTicks( 1 ) ),
 				new JsonWebKeyStub( goodIds[ 1 ] ),
 				new JsonWebKeyStub( goodIds[ 2 ] ),
-				new JsonWebKeyStub( mehIds[ 2 ], DateTime.UtcNow - TimeSpan.FromMilliseconds( 1 ) )
+				new JsonWebKeyStub( mehIds[ 2 ], NOW - TimeSpan.FromTicks( 1 ) )
 			};
 
 			m_mockPublicKeyDataProvider
