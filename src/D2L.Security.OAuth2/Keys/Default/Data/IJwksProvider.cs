8ca9c77f747bd5b7589fceec925fc97f06cdab9b
Jacob Parker <Jacob.Parker@D2L.com>
2017-08-03 16:41:33 +0100

    Add support for bare JWK to IJwkProvider
    This adds a new method IJwkProvider.RequestJwkAsync. It falls back to
    RequestJwksAsync on 404. We want to move to JWK to support better
    caching (i.e. performance) and it is more compatible with services
    like S3+CF that would increase our fault-tolerance.

--- src/D2L.Security.OAuth2/Keys/Default/Data/IJwksProvider.cs
+++ src/D2L.Security.OAuth2/Keys/Default/Data/IJwksProvider.cs
@@ -1,8 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace D2L.Security.OAuth2.Keys.Default.Data {
 	internal interface IJwksProvider {
 		Task<JsonWebKeySet> RequestJwksAsync();
+		Task<JsonWebKey> RequestJwkAsync( Guid keyId );
 		string Namespace { get; }
 	}
 }
