aed5c50fc74a7a93c71093bac228e135d52fc2f9
Jeff Ashton <Jeff.Ashton@D2L.com>
2017-11-13 18:45:06 -0500

    Making Scope immutable

--- src/D2L.Security.OAuth2/Properties/AssemblyInfo.cs
+++ src/D2L.Security.OAuth2/Properties/AssemblyInfo.cs
@@ -32,9 +32,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "5.2.2.0" )]
-[assembly: AssemblyFileVersion( "5.2.3.0" )]
-[assembly: AssemblyInformationalVersion( "5.2.3.0" )]
+[assembly: AssemblyVersion( "6.0.0.0" )]
+[assembly: AssemblyFileVersion( "6.0.0.0" )]
+[assembly: AssemblyInformationalVersion( "6.0.0.0" )]
 
 [assembly: InternalsVisibleTo( "D2L.Security.OAuth2.TestFramework" )]
 [assembly: InternalsVisibleTo( "D2L.Security.OAuth2.Benchmarks" )]
