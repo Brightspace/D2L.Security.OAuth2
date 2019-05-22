6bc3d7d51728f9b079eb727add51ad005ec2c42f
Owen Smith <owen@omsmith.ca>
2019-05-22 10:50:00 -0400

    8.0.2

--- src/D2L.Security.OAuth2.WebApi/Properties/AssemblyInfo.cs
+++ src/D2L.Security.OAuth2.WebApi/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription( "A library that implements Web API components for authenticating D2L services." )]
 [assembly: AssemblyCompany( "Desire2Learn" )]
 [assembly: AssemblyProduct( "Brightspace" )]
-[assembly: AssemblyInformationalVersion( "8.0.1.0" )]
-[assembly: AssemblyVersion( "8.0.1.0" )]
-[assembly: AssemblyFileVersion( "8.0..0" )]
+[assembly: AssemblyInformationalVersion( "8.0.2.0" )]
+[assembly: AssemblyVersion( "8.0.2.0" )]
+[assembly: AssemblyFileVersion( "8.0.2.0" )]
 
 [assembly: AssemblyCopyright( "Copyright © Desire2Learn" )]
 
--- src/D2L.Security.OAuth2/Properties/AssemblyInfo.cs
+++ src/D2L.Security.OAuth2/Properties/AssemblyInfo.cs
@@ -32,9 +32,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "8.0.1.0" )]
-[assembly: AssemblyFileVersion( "8.0.1.0" )]
-[assembly: AssemblyInformationalVersion( "8.0.1.0" )]
+[assembly: AssemblyVersion( "8.0.2.0" )]
+[assembly: AssemblyFileVersion( "8.0.2.0" )]
+[assembly: AssemblyInformationalVersion( "8.0.2.0" )]
 
 [assembly: InternalsVisibleTo( "D2L.Security.OAuth2.TestFramework" )]
 [assembly: InternalsVisibleTo( "D2L.Security.OAuth2.Benchmarks" )]
