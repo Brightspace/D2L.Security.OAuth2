98d682b8532da7448799c7892d505917ec88d1ca
Owen Smith <owen@omsmith.ca>
2020-09-25 17:05:46 -0400

    D2L.Security.OAuth2.TestFramework: modern csproj

--- src/D2L.Security.OAuth2.TestFramework/Properties/AssemblyInfo.cs
+++ src/D2L.Security.OAuth2.TestFramework/Properties/AssemblyInfo.cs
@@ -1,39 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle( "D2L.Security.OAuth2.TestFramework" )]
-[assembly: AssemblyDescription( "Library for obtaining authorization in tests" )]
-[assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "Desire2Learn" )]
-[assembly: AssemblyProduct( "D2L.Security.OAuth2.TestFramework" )]
-[assembly: AssemblyCopyright( "Copyright © Desire2Learn 2018" )]
-[assembly: AssemblyTrademark( "" )]
-[assembly: AssemblyCulture( "" )]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible( false )]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid( "82aa807f-8c33-476f-bd5f-6378f13bae20" )]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "9.0.2.0" )]
-[assembly: AssemblyFileVersion( "9.0.2.0" )]
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo( "D2L.Security.OAuth2.Benchmarks" )]
 [assembly: InternalsVisibleTo( "D2L.Security.OAuth2.UnitTests" )]
