d9e06b88a33b6a0e55efa43744bf6c361040b584
Owen Smith <owen@omsmith.ca>
2021-04-12 18:44:29 -0400

    disable IdentityModel's CryptoProvider caching (#163)

---
+++ src/D2L.Security.OAuth2/Keys/Default/NullCryptoProviderCache.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using D2L.CodeStyle.Annotations;
+using Microsoft.IdentityModel.Tokens;
+
+namespace D2L.Security.OAuth2.Keys.Default {
+	internal sealed class NullCryptoProviderCache : CryptoProviderCache {
+
+		[Statics.Audited(
+			owner: "Owen Smith",
+			auditedDate: "2021-04-12",
+			rationale: "No state"
+		)]
+		internal static readonly NullCryptoProviderCache Instance = new NullCryptoProviderCache();
+
+		private NullCryptoProviderCache() { }
+
+		public override bool TryAdd( SignatureProvider signatureProvider )
+			=> throw new InvalidOperationException();
+		public override bool TryGetSignatureProvider( SecurityKey securityKey, string algorithm, string typeofProvider, bool willCreateSignatures, out SignatureProvider signatureProvider )
+			=> throw new InvalidOperationException();
+		public override bool TryRemove( SignatureProvider signatureProvider )
+			=> throw new InvalidOperationException();
+		protected override string GetCacheKey( SignatureProvider signatureProvider )
+			=> throw new InvalidOperationException();
+		protected override string GetCacheKey( SecurityKey securityKey, string algorithm, string typeofProvider )
+			=> throw new InvalidOperationException();
+	}
+}
