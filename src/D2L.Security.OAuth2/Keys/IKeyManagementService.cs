54cc22120999e23309452cce1e4922a475d54224
Jacob Parker <jacob@solidangle.ca>
2021-09-10 13:07:42 -0400

    Allow private keys to be stored and rotated externally. (#191)
    External storage (vs. in-memory) will work better for
    scale-up/scale-down activity. This will enable us to scale more
    aggressively, including use-cases like AWS Lambda.
    
    It also moves rotation out-of-line of requests, which will avoid
    CPU+latency spikes.
    
    Fetching the latest private key will still be done on-demand, but there is
    the option to do it proactively out-of-band. This will allow us to
    consolidate the fetch in web-server workloads and further hide latency
    from individual requests.
    
    Web server workloads will be able to proactively fetch a key on boot
    before being placed into load.
    
    Co-authored-by: Owen Smith <owen@omsmith.ca>

---
+++ src/D2L.Security.OAuth2/Keys/IKeyManagementService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace D2L.Security.OAuth2.Keys {
+	public interface IKeyManagementService {
+		/// <summary>
+		/// Generates a new key (storing it's public and private keys) if the
+		/// existing ones are getting near their expiry.
+		///
+		/// You would call this in a background job on a regular + frequent
+		/// cadence.
+		/// </summary>
+		Task GenerateNewKeyIfNeededAsync();
+
+		/// <summary>
+		/// Causes the key manager to go looking for a new private key from the
+		/// key store.
+		/// </summary>
+		/// <returns>
+		/// The amount of time to wait before calling this again.
+		/// </returns>
+		Task<TimeSpan> RefreshKeyAsync();
+	}
+}
---
+++ src/D2L.Security.OAuth2/OAuth2Configuration.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace D2L.Security.OAuth2 {
+	public sealed class OAuth2Configuration {
+		/// <summary>
+		/// When a new key is created we try to avoid using it immediately so
+		/// that caches can expire.
+		/// </summary>
+		public TimeSpan KeyTimeUntilUse { get; init; }
+			= TimeSpan.FromHours( 1 );
+
+		/// <summary>
+		/// We rotate keys before they expire so that any tokens signed by the
+		/// key will become invalid before it expires.
+		/// </summary>
+		public TimeSpan KeyRotationBuffer { get; init; }
+			= TimeSpan.FromDays( 7 );
+
+		/// <summary>
+		/// The total lifetime keys we generate.
+		/// </summary>
+		public TimeSpan KeyLifetime { get; init; }
+			= TimeSpan.FromDays( 30 );
+
+		/// <summary>
+		/// Check that the settings make sense and enforce some minimums to
+		/// make reasoning about details around rotation easier.
+		/// </summary>
+		internal void CheckSanity() {
+			if( KeyTimeUntilUse.TotalHours < 1 ) {
+				throw new Exception( "KeyTimeUntilUse is too short" );
+			}
+
+			if( KeyTimeUntilUse >= KeyLifetime ) {
+				throw new Exception( "KeyTimeUntilUse is too long" );
+			}
+
+			if( KeyRotationBuffer.TotalHours < 1 ) {
+				throw new Exception( "KeyRotationBuffer is too short" );
+			}
+
+			if( KeyRotationBuffer >= KeyLifetime ) {
+				throw new Exception( "KeyRotationBuffer is too long" );
+			}
+
+			if( KeyLifetime.TotalHours < 3 ) {
+				throw new Exception( "KeyLifetime is too short" );
+			}
+
+			if( KeyLifetime.TotalDays > 365 ) {
+				throw new Exception( "KeyLifetime is too long" );
+			}
+
+			if( (KeyLifetime - KeyRotationBuffer - KeyTimeUntilUse).TotalHours < 1 ) {
+				throw new Exception( "KeyLifetime should be long enough to avoid rotations for at least an hour" );
+			}
+		}
+	}
+}
