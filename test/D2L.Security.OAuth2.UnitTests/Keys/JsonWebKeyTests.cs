ab706abd4b49646e386e764146421678f1e882f6
Jacob Parker <jacob@solidangle.ca>
2021-10-29 11:25:18 -0400

    Add tests for keys without use

--- test/D2L.Security.OAuth2.UnitTests/Keys/JsonWebKeyTests.cs
+++ test/D2L.Security.OAuth2.UnitTests/Keys/JsonWebKeyTests.cs
@@ -48,6 +48,18 @@ namespace D2L.Security.OAuth2.Keys {
 
 			Assert.DoesNotThrow( () => key.ToSecurityToken() );
 		}
+		[Test]
+		[TestCase( @"{""kid"":""fae33c85-e421-40f5-bebb-8ec8ab778be4"",""kty"":""EC"",""crv"":""P-256"",""x"":""AL09T4hmZ8kGWPSU8mJ-3g3I2kEVZOYhclTWDCNu-0qA"",""y"":""IDCL6vha_57X2KbTO5mkBibZIL7MTi4DGMEDUdM4gnI""}" )]
+		[TestCase( @"{""kid"":""fae33c85-e421-40f5-bebb-8ec8ab778be4"",""kty"":""EC"",""crv"":""P-384"",""x"":""AKfOQlLSk7xXUVaU2kkYUWb0ZvSmIQy3DgJqmYu66TBrFXOMIGvvfEc-qKviYhfKpQ"",""y"":""cCrZCHsHNcnSXw1YoFxiL2qTjVEUf7_Cy4vMTt55CyX3xIxFgkNbd-U4oLi_sgVP""}" )]
+		[TestCase( @"{""kid"":""fae33c85-e421-40f5-bebb-8ec8ab778be4"",""kty"":""EC"",""crv"":""P-521"",""x"":""APMnS32Z3PMZ1cEO6GKi-9VtdVssHyVCWAWdIJl2yuhlmtDgulS8vZ7yjPokw80P0c4eOxlACNrT_8_FiWY5K0l4"",""y"":""Ad58CFD37NRSVJLEgcrxg4ExYFsDZ6Mv-WKjIz3QUVgHo_FksDQ_ZT-7SaKY1DTU64Xc2F9NQpeAOAWySnJIyIQN""}" )]
+		public void FromJson_ParsesStaticEcKeyWithoutUse( string jwk ) {
+			JsonWebKey key = JsonWebKey.FromJson( jwk );
+
+			Assert.AreEqual( "fae33c85-e421-40f5-bebb-8ec8ab778be4", key.Id );
+			Assert.IsFalse( key.ExpiresAt.HasValue );
+
+			Assert.DoesNotThrow( () => key.ToSecurityToken() );
+		}
 
 		[Test]
 		public async Task FromJson_GeneratedKeyRoundTrips() {
