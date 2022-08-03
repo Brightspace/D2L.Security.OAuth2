d7665a6f5c51df840f747e9be32b63a762b04c77
Maggie Sazio <maggie.sazio@D2L.com>
2022-08-03 17:25:09 -0400

    Handle null properties for keys

--- test/D2L.Security.OAuth2.UnitTests/Keys/JsonWebKeyTests.cs
+++ test/D2L.Security.OAuth2.UnitTests/Keys/JsonWebKeyTests.cs
@@ -37,6 +37,17 @@ namespace D2L.Security.OAuth2.Keys {
 		}
 
 		[Test]
+		public void FromJson_ParsesStaticRsaKeyWithNullProperties() {
+			string id = Guid.NewGuid().ToString();
+			string example =
+				@"{""kid"":""" + id + @""",""kty"":""RSA"",""use"":null,""n"":""piXmF9_L0UO4K5APzHqiOYl_KtVXAgPlVHhUopPztaW_JRh2k9MDeupIA1cAF9S_r5qRBWcA1QaP0nlGalw3jm_fSHvtUYYhwUhF9X6I19VRmv_BX9Ne2budt5dafI9DbNs2Ltq0X_yfM1dUL81vaR0rz7jYaQ5bF2CRQHVCcIhWkik85PG5c1yK__As842WqogBpW8-zsEoB6s53FNpDG37_HsZAAngATmTY1At4O7jC6p-c0KVPDf25oLVMOWQubyVgCE9FlsVxprHWqsXenlnHEmhZfEbFB_5KB6hj2yV77jhvLRslNvyKflFBs6AGCiczNDzmoXH2GV3FAVLFQ"",""e"":""AQAB"",""d"":null,""p"":null,""dp"":null,""dq"":null,""qi"":null}";
+
+			JsonWebKey key = JsonWebKey.FromJson(example);
+
+			Assert.AreEqual( id, key.Id );
+		}
+
+		[Test]
 		[TestCase( @"{""kid"":""fae33c85-e421-40f5-bebb-8ec8ab778be4"",""kty"":""EC"",""use"":""sig"",""crv"":""P-256"",""x"":""AL09T4hmZ8kGWPSU8mJ-3g3I2kEVZOYhclTWDCNu-0qA"",""y"":""IDCL6vha_57X2KbTO5mkBibZIL7MTi4DGMEDUdM4gnI""}" )]
 		[TestCase( @"{""kid"":""fae33c85-e421-40f5-bebb-8ec8ab778be4"",""kty"":""EC"",""use"":""sig"",""crv"":""P-384"",""x"":""AKfOQlLSk7xXUVaU2kkYUWb0ZvSmIQy3DgJqmYu66TBrFXOMIGvvfEc-qKviYhfKpQ"",""y"":""cCrZCHsHNcnSXw1YoFxiL2qTjVEUf7_Cy4vMTt55CyX3xIxFgkNbd-U4oLi_sgVP""}" )]
 		[TestCase( @"{""kid"":""fae33c85-e421-40f5-bebb-8ec8ab778be4"",""kty"":""EC"",""use"":""sig"",""crv"":""P-521"",""x"":""APMnS32Z3PMZ1cEO6GKi-9VtdVssHyVCWAWdIJl2yuhlmtDgulS8vZ7yjPokw80P0c4eOxlACNrT_8_FiWY5K0l4"",""y"":""Ad58CFD37NRSVJLEgcrxg4ExYFsDZ6Mv-WKjIz3QUVgHo_FksDQ_ZT-7SaKY1DTU64Xc2F9NQpeAOAWySnJIyIQN""}" )]
