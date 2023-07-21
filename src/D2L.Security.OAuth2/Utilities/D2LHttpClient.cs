fe815e1bbea6a0dbd4eae8e0c82f5f5abf24a68d
AnaCoda <anacod@zoho.com>
2023-07-21 12:42:11 -0600

    Styling and spacing for (I)D2LHttpClient

--- src/D2L.Security.OAuth2/Utilities/D2LHttpClient.cs
+++ src/D2L.Security.OAuth2/Utilities/D2LHttpClient.cs
@@ -3,118 +3,110 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace D2L.Security.OAuth2.Utilities
-{
-	public class D2LHttpClient : ID2LHttpClient
-	{
-		/// <summary>
-		/// This class implements sync versions of HTTPClient methods.
-		/// TODO: Replace the sync methods which are currently using Task.Run(async verison); task.Wait();
-		/// With (probably) HttpWebRequest
-		/// </summary>
+namespace D2L.Security.OAuth2.Utilities {
+	/// <summary>
+	/// This class implements sync versions of HTTPClient methods.
+	/// TODO: Replace the sync methods which are currently using Task.Run(async verison); task.Wait();
+	/// With (probably) HttpWebRequest
+	/// </summary>
+	public class D2LHttpClient : ID2LHttpClient {
 		private readonly HttpClient m_httpClient;
 
 		public D2LHttpClient() {
 			m_httpClient = new HttpClient();
 		}
-		public D2LHttpClient(HttpMessageHandler handler) {
-			m_httpClient = new HttpClient(handler);
+		public D2LHttpClient( HttpMessageHandler handler ) {
+			m_httpClient = new HttpClient( handler );
 		}
-		public D2LHttpClient(HttpMessageHandler handler, bool disposeHandler) { 
-			m_httpClient = new HttpClient(handler, disposeHandler);
+		public D2LHttpClient( HttpMessageHandler handler, bool disposeHandler ) {
+			m_httpClient = new HttpClient( handler, disposeHandler );
 		}
 
-		public Task<HttpResponseMessage> GetAsync(string requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken)
-			=> m_httpClient.GetAsync(requestUri, completionOption, cancellationToken);
-		public Task<HttpResponseMessage> GetAsync(Uri requestUri, CancellationToken cancellationToken)
-			=> m_httpClient.GetAsync(requestUri, cancellationToken);
-		public Task<HttpResponseMessage> GetAsync(Uri requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken)
-			=> m_httpClient.GetAsync(requestUri, completionOption, cancellationToken);
-		public Task<HttpResponseMessage> GetAsync(Uri requestUri, HttpCompletionOption completionOption)
-			=> m_httpClient.GetAsync(requestUri, completionOption);
-		public Task<HttpResponseMessage> GetAsync(string requestUri, HttpCompletionOption completionOption)
-			=> m_httpClient.GetAsync(requestUri, completionOption);
-		public Task<HttpResponseMessage> GetAsync(Uri requestUri)
-			=> m_httpClient.GetAsync(requestUri);
-		public Task<HttpResponseMessage> GetAsync(string requestUri)
-			=> m_httpClient.GetAsync(requestUri);
-		public Task<HttpResponseMessage> GetAsync(string requestUri, CancellationToken cancellationToken)
-			=> m_httpClient.GetAsync(requestUri, cancellationToken);
+		public Task<HttpResponseMessage> GetAsync( string requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken )
+			=> m_httpClient.GetAsync( requestUri, completionOption, cancellationToken );
+		public Task<HttpResponseMessage> GetAsync( Uri requestUri, CancellationToken cancellationToken )
+			=> m_httpClient.GetAsync( requestUri, cancellationToken );
+		public Task<HttpResponseMessage> GetAsync( Uri requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken )
+			=> m_httpClient.GetAsync( requestUri, completionOption, cancellationToken );
+		public Task<HttpResponseMessage> GetAsync( Uri requestUri, HttpCompletionOption completionOption )
+			=> m_httpClient.GetAsync( requestUri, completionOption );
+		public Task<HttpResponseMessage> GetAsync( string requestUri, HttpCompletionOption completionOption )
+			=> m_httpClient.GetAsync( requestUri, completionOption );
+		public Task<HttpResponseMessage> GetAsync( Uri requestUri )
+			=> m_httpClient.GetAsync( requestUri );
+		public Task<HttpResponseMessage> GetAsync( string requestUri )
+			=> m_httpClient.GetAsync( requestUri );
+		public Task<HttpResponseMessage> GetAsync( string requestUri, CancellationToken cancellationToken )
+			=> m_httpClient.GetAsync( requestUri, cancellationToken );
 
-		public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
-			=> m_httpClient.SendAsync(request);
-
-		public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-			=> m_httpClient.SendAsync(request, cancellationToken);
-
-		public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, HttpCompletionOption completionOption)
-			=> m_httpClient.SendAsync(request, completionOption);
-
-		public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, HttpCompletionOption completionOption, CancellationToken cancellationToken)
-			=> m_httpClient.SendAsync(request, completionOption, cancellationToken);
+		public Task<HttpResponseMessage> SendAsync( HttpRequestMessage request )
+			=> m_httpClient.SendAsync( request );
+		public Task<HttpResponseMessage> SendAsync( HttpRequestMessage request, CancellationToken cancellationToken )
+			=> m_httpClient.SendAsync( request, cancellationToken );
+		public Task<HttpResponseMessage> SendAsync( HttpRequestMessage request, HttpCompletionOption completionOption )
+			=> m_httpClient.SendAsync( request, completionOption );
+		public Task<HttpResponseMessage> SendAsync( HttpRequestMessage request, HttpCompletionOption completionOption, CancellationToken cancellationToken )
+			=> m_httpClient.SendAsync( request, completionOption, cancellationToken );
 
 #pragma warning disable D2L0018 // Avoid using dangerous methods
-		public HttpResponseMessage Get(string requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken) {
-			var task = Task.Run(() => m_httpClient.GetAsync(requestUri, completionOption, cancellationToken));
+		public HttpResponseMessage Get( string requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken ) {
+			var task = Task.Run( () => m_httpClient.GetAsync( requestUri, completionOption, cancellationToken ) );
 			task.Wait();
 			return task.Result;
 		}
-		public HttpResponseMessage Get(Uri requestUri, CancellationToken cancellationToken) {
-			var task = Task.Run(() => m_httpClient.GetAsync(requestUri, cancellationToken));
+		public HttpResponseMessage Get( Uri requestUri, CancellationToken cancellationToken ) {
+			var task = Task.Run( () => m_httpClient.GetAsync( requestUri, cancellationToken ) );
 			task.Wait();
 			return task.Result;
 		}
-		public HttpResponseMessage Get(Uri requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken) {
-			var task = Task.Run(() => m_httpClient.GetAsync(requestUri, completionOption, cancellationToken));
+		public HttpResponseMessage Get( Uri requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken ) {
+			var task = Task.Run( () => m_httpClient.GetAsync( requestUri, completionOption, cancellationToken ) );
 			task.Wait();
 			return task.Result;
 		}
-		public HttpResponseMessage Get(Uri requestUri, HttpCompletionOption completionOption) {
-			var task = Task.Run(() => m_httpClient.GetAsync(requestUri, completionOption));
+		public HttpResponseMessage Get( Uri requestUri, HttpCompletionOption completionOption ) {
+			var task = Task.Run( () => m_httpClient.GetAsync( requestUri, completionOption ) );
 			task.Wait();
 			return task.Result;
 		}
-		public HttpResponseMessage Get(string requestUri, HttpCompletionOption completionOption) {
-			var task = Task.Run(() => m_httpClient.GetAsync(requestUri, completionOption));
+		public HttpResponseMessage Get( string requestUri, HttpCompletionOption completionOption ) {
+			var task = Task.Run( () => m_httpClient.GetAsync( requestUri, completionOption ) );
 			task.Wait();
 			return task.Result;
 		}
-		public HttpResponseMessage Get(Uri requestUri) {
-			var task = Task.Run(() => m_httpClient.GetAsync(requestUri));
+		public HttpResponseMessage Get( Uri requestUri ) {
+			var task = Task.Run( () => m_httpClient.GetAsync( requestUri ) );
 			task.Wait();
 			return task.Result;
 		}
-		public HttpResponseMessage Get(string requestUri) {
-			var task = Task.Run(() => m_httpClient.GetAsync(requestUri));
+		public HttpResponseMessage Get( string requestUri ) {
+			var task = Task.Run( () => m_httpClient.GetAsync( requestUri ) );
 			task.Wait();
 			return task.Result;
 		}
-		public HttpResponseMessage Get(string requestUri, CancellationToken cancellationToken) {
-			var task = Task.Run(() => m_httpClient.GetAsync(requestUri, cancellationToken));
-			task.Wait();
-			return task.Result;
-		}
-
-		public HttpResponseMessage Send(HttpRequestMessage request) {
-			var task = Task.Run(() => m_httpClient.SendAsync(request));
+		public HttpResponseMessage Get( string requestUri, CancellationToken cancellationToken ) {
+			var task = Task.Run( () => m_httpClient.GetAsync( requestUri, cancellationToken ) );
 			task.Wait();
 			return task.Result;
 		}
 
-		public HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken) {
-			var task = Task.Run(() => m_httpClient.SendAsync(request, cancellationToken));
+		public HttpResponseMessage Send( HttpRequestMessage request ) {
+			var task = Task.Run( () => m_httpClient.SendAsync( request ) );
 			task.Wait();
 			return task.Result;
 		}
-
-		public HttpResponseMessage Send(HttpRequestMessage request, HttpCompletionOption completionOption) {
-			var task = Task.Run(() => m_httpClient.SendAsync(request, completionOption));
+		public HttpResponseMessage Send( HttpRequestMessage request, CancellationToken cancellationToken ) {
+			var task = Task.Run( () => m_httpClient.SendAsync( request, cancellationToken ) );
 			task.Wait();
 			return task.Result;
 		}
-
-		public HttpResponseMessage Send(HttpRequestMessage request, HttpCompletionOption completionOption, CancellationToken cancellationToken) {
-			var task = Task.Run(() => m_httpClient.SendAsync(request, completionOption, cancellationToken));
+		public HttpResponseMessage Send( HttpRequestMessage request, HttpCompletionOption completionOption ) {
+			var task = Task.Run( () => m_httpClient.SendAsync( request, completionOption ) );
+			task.Wait();
+			return task.Result;
+		}
+		public HttpResponseMessage Send( HttpRequestMessage request, HttpCompletionOption completionOption, CancellationToken cancellationToken ) {
+			var task = Task.Run( () => m_httpClient.SendAsync( request, completionOption, cancellationToken ) );
 			task.Wait();
 			return task.Result;
 		}
