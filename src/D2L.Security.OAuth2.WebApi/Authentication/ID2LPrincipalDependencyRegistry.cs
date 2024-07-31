0d2fd7fb8c71d47d3786ba9ccd2c6091c0074e54
Jacob Parker <jacob@solidangle.ca>
2024-07-31 11:32:44 -0400

    Remove unused WebApi library (#365)

--- src/D2L.Security.OAuth2.WebApi/Authentication/ID2LPrincipalDependencyRegistry.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.Http.Filters;
-using D2L.Security.OAuth2.Principal;
-
-namespace D2L.Security.OAuth2.Authentication {
-	public interface ID2LPrincipalDependencyRegistry {
-		void Register( HttpAuthenticationContext context, ID2LPrincipal principal );
-	}
-}
