04e820f602f811745330264ad2b771ac4912c438
Jacob Parker <jacob@solidangle.ca>
2024-11-29 12:10:53 -0500

    Remove ClaimSet helper (#372)

--- src/D2L.Security.OAuth2/Provisioning/IAccessTokenProvider.cs
+++ src/D2L.Security.OAuth2/Provisioning/IAccessTokenProvider.cs
@@ -6,29 +6,11 @@ using D2L.Security.OAuth2.Caching;
 using D2L.Security.OAuth2.Scopes;
 
 namespace D2L.Security.OAuth2.Provisioning {
-
 	/// <summary>
 	/// Provisions access tokens from the auth service
 	/// </summary>
 	/// <remarks>This type is disposable</remarks>
 	public partial interface IAccessTokenProvider {
-
-		/// <summary>
-		/// Provisions an access token containing the provided claims and scopes.
-		/// </summary>
-		/// <param name="claimSet">The set of claims to be included in the token.</param>
-		/// <param name="scopes">The set of scopes to be included in the token.</param>
-		/// <param name="cache">The provided <see cref="ICache"/> does not need to 
-		/// check for token expiration or grace period because the 
-		/// <see cref="IAccessTokenProvider"/> will handle it internally.</param>
-		/// <returns>An access token containing an expiry and the provided claims and scopes.</returns>
-		[GenerateSync]
-		Task<IAccessToken> ProvisionAccessTokenAsync(
-			ClaimSet claimSet,
-			IEnumerable<Scope> scopes,
-			ICache cache = null
-		);
-
 		/// <summary>
 		/// Provisions an access token containing the provided claims and scopes.
 		/// </summary>
