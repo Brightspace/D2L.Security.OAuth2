1d5c816427135087a237ec0f4f2dadc168db79ea
Jacob Parker <jacob@solidangle.ca>
2026-01-06 10:05:38 -0500

    Improve error message for catch-all validation exceptions

--- src/D2L.Security.OAuth2/Validation/AccessTokens/AccessTokenValidator.cs
+++ src/D2L.Security.OAuth2/Validation/AccessTokens/AccessTokenValidator.cs
@@ -93,10 +93,11 @@ namespace D2L.Security.OAuth2.Validation.AccessTokens {
 			} catch( SecurityTokenNotYetValidException e ) {
 				throw new ValidationException( "Token is from the future (nbf)", e );
 			} catch( Exception e ) {
-				throw new ValidationException( "Unknown validation exception", e );
+				throw new ValidationException( $"Unknown validation exception {e.GetType().FullName}: {e.Message}", e );
 			}
 
 			return accessToken;
 		}
 	}
 }
+
