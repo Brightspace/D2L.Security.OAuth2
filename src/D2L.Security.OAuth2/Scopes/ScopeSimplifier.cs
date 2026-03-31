132490d15dc3e9086c174decec07c4fe0b87ba51
Jacob Parker <jacob@solidangle.ca>
2026-03-31 12:28:03 -0400

    Add ScopeSimplifier
    This comes from the internal oauth2 repo. I don't love the implementation but there are some tests/it is used in the real world, so I'm just copy-pasting it.
    
    I will update the internal usages (this is used to simplify scope in these scenarios:
    * Registering new clients
    * Consenting to applications
    * Issuing tokens
    
    The new use-case is for generating an OpenAPI spec.

---
+++ src/D2L.Security.OAuth2/Scopes/ScopeSimplifier.cs
@@ -0,0 +1,99 @@
+using System.Globalization;
+
+namespace D2L.Security.OAuth2.Scopes;
+
+public static class ScopeSimplifier {
+	public static IEnumerable<Scope> Simplify( this IEnumerable<Scope> @this ) {
+		if( @this.Count() == 1 ) {
+			return @this;
+		}
+
+		var flatScopes = @this.Flatten().Distinct().OrderBy( s => s.ToString() );
+		var set = new HashSet<string>( flatScopes.Select( s => s.ToString() ) );
+
+		var uniqueScopes = flatScopes.Where( s => !set.HasBroaderVariant( s ) );
+
+		var tree = new Dictionary<string, IDictionary<string, IList<string>>>();
+		foreach( var scope in uniqueScopes ) {
+			if( !tree.ContainsKey( scope.Group ) ) {
+				tree.Add( scope.Group, new Dictionary<string, IList<string>>() );
+			}
+
+			var group = tree[ scope.Group ];
+
+			if( !group.ContainsKey( scope.Resource ) ) {
+				group.Add( scope.Resource, new List<string>() );
+			}
+
+			var resource = group[ scope.Resource ];
+
+			resource.Add( scope.Permissions[ 0 ] );
+		}
+
+		return tree
+			.SelectMany(
+				group => group.Value.Select(
+					resource => new Scope( group.Key, resource.Key, resource.Value.ToArray() )
+				)
+			);
+	}
+
+	private static string FormatScope( string group, string resource, string permission ) {
+		return string.Format( "{0}:{1}:{2}", group, resource, permission, CultureInfo.InvariantCulture );
+	}
+
+	private static bool HasBroaderVariant( this HashSet<string> set, Scope scope ) {
+		var scopeAsString = scope.ToString();
+
+		if( set.VariantIsDifferentAndPresent( scopeAsString, FormatScope( "*", "*", "*" ) ) ) {
+			return true;
+		}
+
+		if( set.VariantIsDifferentAndPresent( scopeAsString, FormatScope( "*", "*", scope.Permissions[0] ) ) ) {
+			return true;
+		}
+
+		if( set.VariantIsDifferentAndPresent( scopeAsString, FormatScope( "*", scope.Resource, "*" ) ) ) {
+			return true;
+		}
+
+		if( set.VariantIsDifferentAndPresent( scopeAsString, FormatScope( "*", scope.Resource, scope.Permissions[0] ) ) ) {
+			return true;
+		}
+
+		if( set.VariantIsDifferentAndPresent( scopeAsString, FormatScope( scope.Group, "*", "*" ) ) ) {
+			return true;
+		}
+
+		if( set.VariantIsDifferentAndPresent( scopeAsString, FormatScope( scope.Group, scope.Resource, "*" ) ) ) {
+			return true;
+		}
+
+		if( set.VariantIsDifferentAndPresent( scopeAsString, FormatScope( scope.Group, "*", scope.Permissions[0] ) ) ) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private static bool VariantIsDifferentAndPresent( this HashSet<string> set, string scope, string variant ) {
+		if( scope == variant ) {
+			return false;
+		}
+
+		return set.Contains( variant );
+	}
+
+	private static IEnumerable<Scope> Flatten( this IEnumerable<Scope> @this ) {
+		foreach( Scope scope in @this ) {
+			if( scope.Permissions.Count == 1 ) {
+				yield return scope;
+				continue;
+			}
+
+			foreach( string permission in scope.Permissions ) {
+				yield return new Scope( scope.Group, scope.Resource, permission );
+			}
+		}
+	}
+}
---
+++ test/D2L.Security.OAuth2.UnitTests/Scopes/ScopeSimplifierTests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+
+namespace D2L.Security.OAuth2.Scopes;
+
+[TestFixture]
+internal sealed class ScopeSimplifierTests {
+	[TestCase( "foo:bar:baz", "foo:bar:baz", TestName = "Should map single scope simply" )]
+	[TestCase( "foo:bar:baz foo:bar:baz", "foo:bar:baz", TestName = "Should remove a simple duplicate" )]
+	[TestCase( "foo:bar:d,c", "foo:bar:c,d", TestName = "Should alphabetize permissions" )]
+	[TestCase( "foo:bar:baz foo:bar:quux", "foo:bar:baz,quux", TestName = "Should combine permissions of single resource" )]
+	[TestCase( "foo:bar:baz,foozle foo:bar:baz,quux", "foo:bar:baz,foozle,quux", TestName = "Should remove duplicates when combining permissions" )]
+	[TestCase( "foo:bar:baz,foozle foo:bar:baz,quux foo:bar:*", "foo:bar:*", TestName = "Should simplify to wildcard permission if present" )]
+	[TestCase( "foo:bar:baz foo:quux:baz", "foo:bar:baz foo:quux:baz", TestName = "Should map multi resource simply" )]
+	[TestCase( "foo:quux:baz foo:bar:baz", "foo:bar:baz foo:quux:baz", TestName = "Should alphabetize multi resource" )]
+	[TestCase( "foo:bar:baz foo:quux:puppy foo:bar:foozle, foo:quux:baz", "foo:bar:baz,foozle foo:quux:baz,puppy", TestName = "Should combine permissions of individual resources" )]
+	[TestCase( "foo:bar:baz foo:quux:baz foo:bar:baz", "foo:bar:baz foo:quux:baz", TestName = "Should remove duplicates with multiple resources" )]
+	[TestCase( "foo:bar:baz,foozle foo:quux:baz foo:*:baz", "foo:*:baz foo:bar:foozle", TestName = "Should use wildcard resource permissions if present" )]
+	[TestCase( "foo:bar:baz quux:foozle:puppy", "foo:bar:baz quux:foozle:puppy", TestName = "Should map multi group simply" )]
+	[TestCase( "quux:foozle:puppy foo:bar:baz", "foo:bar:baz quux:foozle:puppy", TestName = "Should alphabetize multi group" )]
+	[TestCase( "foo:bar:baz quux:foozle:puppy foo:bar:kitty quux:foozle:duck", "foo:bar:baz,kitty quux:foozle:duck,puppy", TestName = "Should combine permissions of individual groups/resources" )]
+	[TestCase( "foo:bar:baz quux:foozle:puppy foo:bar:baz", "foo:bar:baz quux:foozle:puppy", TestName = "Should remove duplicates with multiple groups" )]
+	[TestCase( "foo:bar:baz,kitty quux:bar:puppy,baz *:bar:baz", "*:bar:baz foo:bar:kitty quux:bar:puppy", TestName = "Should use wildcard group permissions if present" )]
+	[TestCase( "foo:bar:baz,kitty quux:foozle:puppy,baz *:*:baz", "*:*:baz foo:bar:kitty quux:foozle:puppy", TestName = "Should use wildcard group permissions if present (2)" )]
+	[TestCase( "foo:bar:baz,kitty quux:bar:puppy,baz *:*:*", "*:*:*", TestName = "Super-scope should be super-simple" )]
+	public void Test( string input, string expected ) {
+		Assert.That(
+			expected,
+			Is.EqualTo( string.Join( " ", ScopeSimplifier.Simplify( ScopeValidator.ParseScopes( input ) ) ) )
+		);
+	}
+}
